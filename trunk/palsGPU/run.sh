431e14242019ff3a9dbb1418339605fe9b482aac
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-10-30 19:41:34 +0000

    Refactoreo. Se agrega PALS serial

--- trunk/palsGPU/run.sh
+++ trunk/palsGPU/run.sh
@@ -1,4 +1,4 @@
 make clean
 make all
-bin/pals_gpu data/A.u_c_hihi_256x16 256 16
+bin/pals data/A.u_c_hihi_256x16 256 16 1
 #bin/pals_gpu data/A.u_c_hihi_1024x32 1024 32
--- trunk/palsGPU/src/load_params.h
+++ trunk/palsGPU/src/load_params.h
@@ -8,10 +8,15 @@
 #ifndef LOAD_PARAMS_H_
 #define LOAD_PARAMS_H_
 
+#define PALS_Serial 0
+#define PALS_GPU 1
+
 struct params {
 	char *instance_path;
 	int machines_count;
 	int tasks_count;
+	
+	int pals_flavour;
 };
 
 int load_params(int argc, char **argv, struct params *input);
