09c891c121a74d6a56dca0f508bb1e82b0ed5116
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-10-31 01:52:21 +0000

    verga?

--- trunk/palsGPU/src/pals_gpu.cu
+++ trunk/palsGPU/src/pals_gpu.cu
@@ -147,7 +147,7 @@ __global__ void pals_kernel(int task_count, int block_size,
 	// Coordenadas del swap.
 	auxf = block_offset_start + (thread_idx/task_count);
 	int current_swap_coord_x = (int)auxf;
-	int current_swap_coord_y = (int)(((int)auxf - auxf) * task_count);
+	int current_swap_coord_y = (int)((current_swap_coord_x - auxf) * task_count);
 
 	// El primer task_per_thread siempre debería tener un swap válido.
 	// Calculo el delta de ese primer swap y lo dejo como mejor.		
@@ -167,9 +167,9 @@ __global__ void pals_kernel(int task_count, int block_size,
 	// Para todos los demás task_per_thread.
 	// En caso de que task_per_thread = 1, esto nunca se ejecuta y nunca hay divergencia de código.
 	for (i = 1; i < tasks_per_thread; i++) {
-		auxf = block_offset_start  + ((block_size * i)/task_count) + (thread_idx/task_count);
+		auxf = block_offset_start  + (((block_size * i) + thread_idx) / task_count);
 		current_swap_coord_x = (int)auxf;
-		current_swap_coord_y = (int)(((int)auxf - auxf) * task_count);
+		current_swap_coord_y = (int)((current_swap_coord_x - auxf) * task_count);
 
 		// Si la cantidad de tareas no es divisible entre la cantidad de threads
 		// per block, el último bloque puede tener threads sobrantes. En este
