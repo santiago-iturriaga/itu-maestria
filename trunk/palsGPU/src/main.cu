dde95d39c49f15360c65eaa03512a2809f759ec6
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-10-31 00:17:53 +0000

    Funciona?

--- trunk/palsGPU/src/main.cu
+++ trunk/palsGPU/src/main.cu
@@ -117,24 +117,18 @@ int main(int argc, char** argv)
 	
 		// Debug ------------------------------------------------------------------------------------------
 		if (DEBUG) {
-			long current_swap;
+			int current_swap;
 			int task_x;
 			int machine_a;
 			int task_y;
 			int machine_b;
 	
 			fprintf(stdout, "[DEBUG] Mejores swaps:\n");
-			for (int block_idx = 0; block_idx < instance.number_of_blocks; block_idx++) {
-				int thread_idx = (int)(best_swaps[block_idx] / instance.tasks_per_thread);
-				int task_idx = (int)(best_swaps[block_idx] % instance.tasks_per_thread);
-			
-				current_swap = ((long)instance.block_size * (long)instance.tasks_per_thread * (long)block_idx) 
-					+ ((long)instance.block_size * (long)task_idx) + (long)thread_idx;
-				
-				task_x = (int)(current_swap / etc_matrix->tasks_count);
-				task_y = (int)(current_swap % etc_matrix->tasks_count);
-				
+			for (int i = 0; i < instance.number_of_blocks; i++) {
+				current_swap = best_swaps[i];
+				task_x = (int)floor((float)current_swap / (float)etc_matrix->tasks_count);
 				machine_a = current_solution->task_assignment[task_x];
+				task_y = (int)fmod((float)current_swap, (float)etc_matrix->tasks_count);
 				machine_b = current_solution->task_assignment[task_y];
 	
 				float swap_delta = 0.0;
@@ -143,8 +137,8 @@ int main(int argc, char** argv)
 				swap_delta -= get_etc_value(etc_matrix, machine_b, task_y); // Resto el ETC de y en b.
 				swap_delta += get_etc_value(etc_matrix, machine_b, task_x); // Sumo el ETC de x en b.
 	
-				fprintf(stdout, "   Swap ID %ld. Delta %f (%f). Task %d in %d swaps with task %d in %d.\n", 
-					current_swap, best_swaps_delta[block_idx], swap_delta, task_x, machine_a, task_y, machine_b);
+				fprintf(stdout, "   Swap ID %d. Delta %f (%f). Task %d in %d swaps with task %d in %d.\n", 
+					current_swap, best_swaps_delta[i], swap_delta, task_x, machine_a, task_y, machine_b);
 			}
 		}
 		// Debug ------------------------------------------------------------------------------------------
