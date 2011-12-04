e762b6a6ccc36975f92a00aacc4bc32bac3889a5
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-12-04 16:41:02 +0000

    Cambio en la función de fitness

--- trunk/palsGPU/src/config.h
+++ trunk/palsGPU/src/config.h
@@ -4,12 +4,12 @@
 #define DEBUG 1
 #define TIMMING 1
 
-#define PALS_RTASK_RANDS 6144*2
+#define PALS_RTASK_RANDS 6144*5
 
 //#define PALS_COUNT 1
 //#define PALS_COUNT 5
-//#define PALS_COUNT 25
-#define PALS_COUNT 50000
+#define PALS_COUNT 25
+//#define PALS_COUNT 50000
 //#define PALS_COUNT 100000
 //#define PALS_COUNT 1000000
 
