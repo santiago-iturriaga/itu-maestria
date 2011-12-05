6f16d2d972a694878edfd76a8611017b3a518f6e
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-12-05 04:14:51 +0000

    No anda el LOOP, lo deshabilité. Buenos resultados de 4k en 9s

--- palsGPU/src/config.h
+++ palsGPU/src/config.h
@@ -4,12 +4,14 @@
 #define DEBUG 1
 #define TIMMING 1
 
-#define PALS_RTASK_RANDS 6144*5
+#define PALS_RTASK_RANDS 6144*20
 
 //#define PALS_COUNT 1
 //#define PALS_COUNT 5
 //#define PALS_COUNT 25
-#define PALS_COUNT 1000
+//#define PALS_COUNT 2500
+#define PALS_COUNT 7000
+//#define PALS_COUNT 10000
 //#define PALS_COUNT 50000
 //#define PALS_COUNT 100000
 //#define PALS_COUNT 1000000
