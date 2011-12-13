f44698826c66edff244467372a4ef24a60aa9153
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2011-12-13 22:25:05 +0000

    auxiliares

--- palsGPU/verificador.c
+++ palsGPU/verificador.c
@@ -15,9 +15,9 @@
 
 int main(int argc, char *argv[]){
 
-if (argc < 3){
-printf("Sintaxis: %s <archivo_instancias> <archivo_sol>\n", argv[0]);
-exit(1);
+if (argc < 5){
+	printf("Sintaxis: %s <archivo_instancias> <archivo_sol> <#tasks> <#machines>\n", argv[0]);
+	exit(1);
 }
 
 int NT, NM;
@@ -41,36 +41,39 @@ printf("Archivo sol: %s\n",arch_sol);
 // Leer archivo, almacenando matriz ETC
 
 if((fi=fopen(arch_inst, "r"))==NULL){
-printf("No se puede leer archivo de instancia %s\n",arch_inst);
-exit(1);
+	printf("No se puede leer archivo de instancia %s\n",arch_inst);
+	exit(1);
 }
 
 int i,j;
 
-fscanf(fi,"%d %d",&NT,&NM);
+NT=atoi(argv[3]);
+NM=atoi(argv[4]);
+
+//fscanf(fi,"%d %d",&NT,&NM);
 printf("NT: %d, NM: %d\n",NT,NM);
 
 float **ETC = (float **) malloc(sizeof(float *)*NT);
 
 if (ETC == NULL){
-printf("Error al reservar memoria para ETC, dimensiones %dx%d\n",NT,NM);
-exit(2);
+	printf("Error al reservar memoria para ETC, dimensiones %dx%d\n",NT,NM);
+	exit(2);
 }
 
 for (i=0;i<NT;i++){
-ETC[i] = (float *) malloc(sizeof(float)*NM);
-if (ETC[i] == NULL){
-printf("Error al reservar memoria para fila %d de ETC\n",i);
-exit(2);
-}
+	ETC[i] = (float *) malloc(sizeof(float)*NM);
+	if (ETC[i] == NULL){
+		printf("Error al reservar memoria para fila %d de ETC\n",i);
+		exit(2);
+	}
 }
 
 int max_etc=0;
 
 for (i=0;i<NT;i++){
-for (j=0;j<NM;j++){
-fscanf(fi,"%f",&ETC[i][j]);
-}
+	for (j=0;j<NM;j++){
+		fscanf(fi,"%f",&ETC[i][j]);
+	}
 }
 
 close(fi);
@@ -78,45 +81,45 @@ close(fi);
 // Array de maquinas, almacena el MAT
 float *mach = (float *) malloc(sizeof(float)*NM);
 for (j=0;j<NM;j++){
-mach[j]=0.0;
+	mach[j]=0.0;
 }
 
 // Array de asignaciones
 int *asig= (int*) malloc(sizeof(float)*NT);
 int nro_asig=0;
 for (i=0;i<NT;i++){
-asig[i]=NO_ASIG;
+	asig[i]=NO_ASIG;
 }
 
 if((fs=fopen(arch_sol, "r"))==NULL){
-printf("No se puede leer archivo de solucion %s\n",arch_sol);
-exit(1);
+	printf("No se puede leer archivo de solucion %s\n",arch_sol);
+	exit(1);
 }
 
 for (i=0;i<NT;i++){
-fscanf(fs,"%d ",&asig[i]);
+	fscanf(fs,"%d ",&asig[i]);
 }
 
 fclose(fs);
 
 printf("Sol: [");
 for (i=0;i<NT;i++){
-printf("%d ",asig[i]);
+	printf("%d ",asig[i]);
 }
 printf("]\n");
 
 int maq;
 
 for(i=0;i<NT;i++){
-maq = asig[i];
-mach[maq] = mach[maq]+ETC[i][maq];
+	maq = asig[i];
+	mach[maq] = mach[maq]+ETC[i][maq];
 }
 
 float makespan = 0.0;
 for (j=0;j<NM;j++){
-if (mach[j]>makespan){
-makespan = mach[j];
-}
+	if (mach[j]>makespan){
+		makespan = mach[j];
+	}
 }
 
 printf("Makespan: %f\n",makespan);
