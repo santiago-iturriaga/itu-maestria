f9ed06f4059e585726e79e3938769ea219201408
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-04 21:55:51 +0000

    Se eliminan los branches

--- branches/palsGPU-byMachine/debug.sh
+++
@@ -1,4 +0,0 @@
-make clean
-make all
-gdb bin/pals data/A.u_c_hihi_256x16 256 16 1
-#bin/pals_gpu data/A.u_c_hihi_1024x32 1024 32
--- branches/palsGPU-byMachine/run_65536x2048.sh
+++
@@ -1,3 +0,0 @@
-make clean
-make all
-bin/pals data/A.u_c_hihi_65536x2048 65536 2048 1
