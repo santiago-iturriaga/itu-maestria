c4165e23b03758f1612c4e2792d062fcba1885cb
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-05 03:45:02 +0000

    Scripts hasta 8192

--- trunk/palsGPU/1024x32/A.u_c_hihi.minmin.sh
+++
@@ -1,2 +0,0 @@
-time (../bin/pals /home/sergion/LUX/instancias/1024x32/A.u_c_hihi 1024 32 4 0 3 > A.u_c_hihi.minmin.log)
-
--- trunk/palsGPU/8192x256/A.u_c_hihi.fixed.sh
+++
@@ -1,2 +0,0 @@
-time (../bin/pals /home/sergion/LUX/instancias/8192x256/A.u_c_hihi 8192 256 2 0 3 > A.u_c_hihi.fixed.log)
-
