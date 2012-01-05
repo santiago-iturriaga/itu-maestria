796a55dada2aceced7f62a26acd468d30754f87f
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-05 22:21:23 +0000

    1er versión estable. Generé datos hasta 2048x64. Tiene un bug y muy de vez en cuando se cae.

--- aprend_aut/articulo_final/java/AAI/train_crf_full.sh
+++
@@ -1,7 +0,0 @@
-#rm model_crf/*.model
-
-for i in {0..9}
-do
-	java -classpath /home/santiago/eclipse/java-workspace/AAI/bin:/home/santiago/eclipse/java-workspace/AAI/lib/mallet-deps.jar \
-		-Xmx3072M AII.FullCRFTrain ${i}
-done
