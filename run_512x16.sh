7d47cb5b864e0269197d5fcb8dfaf5afe31c8a5a
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-16 03:48:52 +0000

    Orientacion maquina

---
+++ run_512x16.sh
@@ -0,0 +1,5 @@
+#make clean
+#make pals-gdb
+bin/pals_cpu /home/santiago/eclipse/c-c++-workspace/AE/ProblemInstances/HCSP/1024x32/A.u_c_hihi 512 16 3 3 0
+bin/pals_cpu /home/santiago/eclipse/c-c++-workspace/AE/ProblemInstances/HCSP/1024x32/A.u_c_hihi 512 16 4
+
