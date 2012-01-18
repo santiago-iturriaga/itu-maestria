aa1829c38c56d1b095c0fd4a4a969f01dcc92ccf
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-18 04:13:45 +0000

    Procesamiento de energía

--- branches/palsCPU/src/solution.cpp
+++ branches/palsCPU/src/solution.cpp
@@ -86,9 +86,21 @@ void init_empty_solution(struct etc_matrix *etc, struct energy_matrix *energy, s
 }
 
 void clone_solution(struct solution *dst, struct solution *src) {
-	dst->__makespan = src->__makespan;
 	memcpy(dst->__task_assignment, src->__task_assignment, sizeof(int) * src->etc->tasks_count);
+	for (int machine = 0; machine < src->etc->machines_count; machine++) {
+		memcpy(dst->__machine_assignment[machine], src->__machine_assignment[machine], sizeof(int) * src->etc->tasks_count);
+	}
+   	memcpy(dst->__machine_assignment_count, src->__machine_assignment_count, sizeof(int) * src->etc->machines_count);
+    
+    // Makespan
 	memcpy(dst->__machine_compute_time, src->__machine_compute_time, sizeof(float) * src->etc->machines_count);
+	dst->__worst_ct_machine_id = src->__worst_ct_machine_id;
+	dst->__makespan = src->__makespan;
+	
+	// Energy
+	memcpy(dst->__machine_energy_consumption, src->__machine_energy_consumption, sizeof(float) * src->etc->machines_count);
+	dst->__worst_energy_machine_id = src->__worst_energy_machine_id;
+	dst->__total_energy_consumption = src->__total_energy_consumption;
 }
 
 void free_solution(struct solution *s) {
@@ -110,9 +122,29 @@ void assign_task_to_machine(struct solution *s, int machine_id, int task_id) {
     assert(task_id >= 0);
     assert(s->__task_assignment[task_id] == TASK__NOT_ASSIGNED);
        
+    // Actualizo el makespan.
     s->__machine_compute_time[machine_id] += get_etc_value(s->etc, machine_id, task_id);
-    if (s->__machine_compute_time[machine_id] > s->__makespan) s->__makespan = s->__machine_compute_time[machine_id];
+    if (s->__machine_compute_time[machine_id] > s->__makespan) {
+        s->__makespan = s->__machine_compute_time[machine_id];
+        s->__worst_ct_machine_id = machine_id;
+    }
     
+    // Actualizo la energía.
+    float old_machine_energy = s->__machine_energy_consumption[machine_id];
+    float new_machine_energy = (s->__machine_compute_time[machine_id] * get_energy_max_value(s->energy, machine_id))
+        + ((s->__makespan - s->__machine_compute_time[machine_id]) * get_energy_idle_value(s->energy, machine_id));
+        
+    s->__machine_energy_consumption[machine_id] = new_machine_energy;
+    
+    if (machine_id == s->__worst_energy_machine_id) {
+        // Sigue siendo la peor máquina.    
+    } else if (new_machine_energy > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+        s->__worst_energy_machine_id = machine_id;
+    }
+    
+    s->__total_energy_consumption = s->__total_energy_consumption - old_machine_energy + new_machine_energy;
+    
+    // Asigno la tarea.
     s->__task_assignment[task_id] = machine_id;
 
 	int current_machine_task_count = s->__machine_assignment_count[machine_id];
@@ -128,7 +160,9 @@ void move_task_to_machine(struct solution *s, int task_id, int machine_id) {
     assert(s->__task_assignment[task_id] != TASK__NOT_ASSIGNED);
     assert(s->__task_assignment[task_id] != machine_id);
     
-    int recompute_makespan = 0;    
+    int recompute_makespan = 0;
+    int recompute_energy = 0;
+    
     int machine_origin_id = s->__task_assignment[task_id];
     
     // Actualizo los compute time.
@@ -136,12 +170,55 @@ void move_task_to_machine(struct solution *s, int task_id, int machine_id) {
     
     if (s->__machine_compute_time[machine_id] > s->__makespan) {
         s->__makespan = s->__machine_compute_time[machine_id];
+        s->__worst_ct_machine_id = machine_id;
+        
+        recompute_energy = 1;
     } else if (s->__machine_compute_time[machine_origin_id] == s->__makespan) {
         recompute_makespan = 1;
     }
     
-    s->__machine_compute_time[machine_origin_id] -= get_etc_value(s->etc, machine_origin_id, task_id);
+    if (s->__machine_compute_time[machine_origin_id] < s->__makespan) {
+        s->__machine_compute_time[machine_origin_id] -= get_etc_value(s->etc, machine_origin_id, task_id);
+    } else {
+        recompute_makespan = 1;
+    }
     
+    // Actualizo la energía.
+    if (recompute_energy == 0) {
+        if (recompute_makespan == 0) {
+            // Máquina destino.
+            float old_machine_energy = s->__machine_energy_consumption[machine_id];
+            float new_machine_energy = (s->__machine_compute_time[machine_id] * get_energy_max_value(s->energy, machine_id))
+                + ((s->__makespan - s->__machine_compute_time[machine_id]) * get_energy_idle_value(s->energy, machine_id));
+                
+            s->__machine_energy_consumption[machine_id] = new_machine_energy;
+            
+            if (machine_id == s->__worst_energy_machine_id) {
+                // Sigue siendo la peor máquina.    
+            } else if (new_machine_energy > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+                s->__worst_energy_machine_id = machine_id;
+            }
+            
+            s->__total_energy_consumption = s->__total_energy_consumption - old_machine_energy + new_machine_energy;
+            
+            // Máquina origen.
+            old_machine_energy = s->__machine_energy_consumption[machine_origin_id];
+            new_machine_energy = (s->__machine_compute_time[machine_origin_id] * get_energy_max_value(s->energy, machine_origin_id))
+                + ((s->__makespan - s->__machine_compute_time[machine_origin_id]) * get_energy_idle_value(s->energy, machine_origin_id));
+                
+            s->__machine_energy_consumption[machine_origin_id] = new_machine_energy;
+            
+            if (machine_origin_id == s->__worst_energy_machine_id) {
+                // Busco el nuevo peor.
+                refresh_worst_energy(s);
+            }
+            
+            s->__total_energy_consumption = s->__total_energy_consumption - old_machine_energy + new_machine_energy;
+        } else {
+            recompute_energy = 1;
+        }
+    }
+        
     // Quito la task a la máquina origen.
     int current_machine_origin_task_count = get_machine_tasks_count(s, machine_origin_id);
     int machine_origin_task_pos = get_machine_task_pos(s, machine_origin_id, task_id);
@@ -161,8 +238,9 @@ void move_task_to_machine(struct solution *s, int task_id, int machine_id) {
 	s->__machine_assignment[machine_id][current_machine_task_count] = task_id;
 	s->__machine_assignment_count[machine_id] = current_machine_task_count + 1;   
     
-    // Si es necesario recalculo el makespan de la solución.
+    // Si es necesario recalculo el makespan o la energía de la solución.
     if (recompute_makespan == 1) refresh_makespan(s);
+    if (recompute_energy == 1) refresh_energy(s);
 }
 
 void swap_tasks(struct solution *s, int task_a_id, int task_b_id) {
@@ -173,7 +251,10 @@ void swap_tasks(struct solution *s, int task_a_id, int task_b_id) {
     assert(s->__task_assignment[task_a_id] != TASK__NOT_ASSIGNED);
     assert(s->__task_assignment[task_b_id] != TASK__NOT_ASSIGNED);
     
-    int recompute_makespan = 0;    
+    int recompute_makespan = 0;
+    int recompute_energy = 0;
+    
+    // Calculo el compute time y el makespan.
     int machine_a_id = s->__task_assignment[task_a_id];
     int machine_b_id = s->__task_assignment[task_b_id];
     
@@ -194,12 +275,64 @@ void swap_tasks(struct solution *s, int task_a_id, int task_b_id) {
         } else {
             s->__makespan = machine_b_ct_new;
         }
-    } else {
-        if (machine_a_ct == s->__makespan || machine_b_ct == s->__makespan) {
-            recompute_makespan = 1;
+        
+        recompute_energy = 1;
+    } else if (machine_a_ct == s->__makespan || machine_b_ct == s->__makespan) {
+        recompute_makespan = 1;
+    }
+    
+    // Calculo la energía.
+    if (recompute_energy == 0) {
+        if (recompute_makespan == 0) {
+            int refresh_worst = 0;
+        
+            // Máquina a.
+            float old_machine_energy = s->__machine_energy_consumption[machine_a_id];
+            float new_machine_energy = (s->__machine_compute_time[machine_a_id] * get_energy_max_value(s->energy, machine_a_id))
+                + ((s->__makespan - s->__machine_compute_time[machine_a_id]) * get_energy_idle_value(s->energy, machine_a_id));
+                
+            s->__machine_energy_consumption[machine_a_id] = new_machine_energy;
+            
+            if (machine_a_id == s->__worst_energy_machine_id) {
+                if (new_machine_energy > old_machine_energy) {
+                    // Sigue siendo la peor máquina.
+                } else {
+                    refresh_worst = 1;
+                }
+            } else if (new_machine_energy > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+                s->__worst_energy_machine_id = machine_a_id;
+            }
+            
+            s->__total_energy_consumption = s->__total_energy_consumption - old_machine_energy + new_machine_energy;
+            
+            // Máquina b.
+            old_machine_energy = s->__machine_energy_consumption[machine_b_id];
+            new_machine_energy = (s->__machine_compute_time[machine_b_id] * get_energy_max_value(s->energy, machine_b_id))
+                + ((s->__makespan - s->__machine_compute_time[machine_b_id]) * get_energy_idle_value(s->energy, machine_b_id));
+                
+            s->__machine_energy_consumption[machine_b_id] = new_machine_energy;
+            
+            if (machine_b_id == s->__worst_energy_machine_id) {
+                if (new_machine_energy > old_machine_energy) {
+                    // Sigue siendo la peor máquina.
+                } else {
+                    refresh_worst = 1;
+                }
+            } else if (new_machine_energy > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+                s->__worst_energy_machine_id = machine_b_id;
+            }
+            
+            s->__total_energy_consumption = s->__total_energy_consumption - old_machine_energy + new_machine_energy;
+            
+            if (refresh_worst == 1) {
+                refresh_worst_energy(s);
+            }
+        } else {
+            recompute_energy = 1;
         }
     }
 
+    // Asigno las tareas.
     int task_a_pos, task_b_pos;
     task_a_pos = get_machine_task_pos(s, machine_a_id, task_a_id);;
     task_b_pos = get_machine_task_pos(s, machine_b_id, task_b_id);;
@@ -210,8 +343,9 @@ void swap_tasks(struct solution *s, int task_a_id, int task_b_id) {
     s->__task_assignment[task_a_id] = machine_b_id;
     s->__task_assignment[task_b_id] = machine_a_id;
 
-    // Si es necesario recalculo el makespan.    
+    // Si es necesario recalculo el makespan o la energía de la solución.
     if (recompute_makespan == 1) refresh_makespan(s);
+    if (recompute_energy == 1) refresh_energy(s);
 }
 
 float get_machine_compute_time(struct solution *s, int machine_id) {
@@ -221,11 +355,55 @@ float get_machine_compute_time(struct solution *s, int machine_id) {
     return s->__machine_compute_time[machine_id];
 }
 
+float get_makespan(struct solution *s) {
+    return s->__makespan;
+}
+
+float get_energy(struct solution *s) {
+    return s->__total_energy_consumption;
+}
+
+int get_worst_ct_machine_id(struct solution *s) {
+    return s->__worst_ct_machine_id;
+}
+
+int get_worst_energy_machine_id(struct solution *s) {
+    return s->__worst_energy_machine_id;
+}
+
+void refresh_energy(struct solution *s) {
+    s->__worst_energy_machine_id = 0;
+    s->__total_energy_consumption = 0.0;
+    
+    for (int i = 0; i < s->etc->machines_count; i++) {
+        s->__machine_energy_consumption[i] = (s->__machine_compute_time[i] * get_energy_max_value(s->energy, i))
+            + ((s->__makespan - s->__machine_compute_time[i]) * get_energy_idle_value(s->energy, i));
+            
+        s->__total_energy_consumption += s->__machine_energy_consumption[i];
+        
+        if (s->__machine_energy_consumption[i] > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+            s->__worst_energy_machine_id = i;
+        }
+    }
+}
+
+void refresh_worst_energy(struct solution *s) {
+    s->__worst_energy_machine_id = 0;
+    
+    for (int i = 1; i < s->etc->machines_count; i++) {
+        if (s->__machine_energy_consumption[i] > s->__machine_energy_consumption[s->__worst_energy_machine_id]) {
+            s->__worst_energy_machine_id = i;
+        }
+    }
+}
+
 void refresh_makespan(struct solution *s) {
+    s->__worst_ct_machine_id = 0;
     s->__makespan = s->__machine_compute_time[0];
     
     for (int i = 1; i < s->etc->machines_count; i++) {
         if (s->__machine_compute_time[i] > s->__makespan) {
+            s->__worst_ct_machine_id = i;
             s->__makespan = s->__machine_compute_time[i];
         }
     }
@@ -276,10 +454,6 @@ int get_machine_task_pos(struct solution *s, int machine_id, int task_id) {
     } else {
         return -1;
     }
-}
-
-float get_makespan(struct solution *s) {
-    return s->__makespan;
 }
 
 void validate_solution(struct solution *s) {
