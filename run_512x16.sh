1e50d8cad454258cd284d748507a6f8f2188af73
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-21 04:12:38 +0000

    Compila. Y parece que hasta anda

--- run_512x16.sh
+++ run_512x16.sh
@@ -1,7 +1,7 @@
 INSTANCES_PATH=/home/santiago/Scheduling/Instances/Makespan-Energy/512x16
 
 #make clean
-make pals-gdb
+#make pals-gdb
 
-bin/pals_cpu ${INSTANCES_PATH}/scenario.0 ${INSTANCES_PATH}/workload.0 3 2 0
+bin/pals_cpu ${INSTANCES_PATH}/scenario.0 ${INSTANCES_PATH}/workload.0 3 4 0
 
