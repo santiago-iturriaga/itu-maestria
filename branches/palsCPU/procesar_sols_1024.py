6d0a8bbb6f87d3381465824c76dd37080d86c55a
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-01-30 00:12:19 +0000

    scripts

--- branches/palsCPU/procesar_sols_1024.py
+++ branches/palsCPU/procesar_sols_1024.py
@@ -3,9 +3,9 @@
 import os
 import math
 
-list_heur = ['MinMin', 'MINMin', 'MinMIN', 'MINMIN']
-list_heur_dir = 'list-heuristics/1024x32'
-palsRuso_dir = 'pals-ruso/1024x32'
+cant_iters=5
+minmin_dir = 'list-heuristics/1024x32'
+pals_ruso_dir = 'pals-ruso/1024x32'
 pals_dir = '1024x32.test'
 
 if __name__ == '__main__':
@@ -13,50 +13,18 @@ if __name__ == '__main__':
 
     for filename in os.listdir(pals_dir):
         nameParts = filename.split('.')
-        instancias_raw.append(nameParts[0] + '.' + nameParts[1])
+        instancias_raw.append((nameParts[1],nameParts[3]+'.'+nameParts[4]))
 
-    ejecuciones_pals = {}
-
-    for instancia in instancias_raw:
-        for filename in os.listdir(pals_dir + '/' + instancia):
-            filename_parts = filename.split('.')
-            
-            if len(filename_parts) == 5:
-                file_inst = filename_parts[2] + '.' + filename_parts[3]
-                file_version = filename_parts[0] + '.' + filename_parts[1]
-                file_type = filename_parts[4]
-                file_id = filename_parts[0] + '.' + filename_parts[1] + '.' + filename_parts[2] + '.' + filename_parts[3]
-                
-                if not file_version in ejecuciones_pals:
-                    ejecuciones_pals[file_inst] = []
-                    ejecuciones_pals[file_inst].append(file_id)
-                             
     instancias = list(set(instancias_raw))
     instancias.sort()
 
-    resultados_list_heur = []
+    resultados_minmin = {}
     resultados_pals = {}
-    resultados_palsRuso = {}
+    resultados_pals_ruso = {}
 
     for instancia in instancias:
-        for d in list_heur:
-            path = list_heur_dir + '/' + d + '.' + instancia + '.metrics'
-            print path
-            
-            if os.path.isfile(path):
-                metrics_file = open(path)
-                values = metrics_file.readline().split(' ')
-                makespan = float(values[0])
-                energy = float(values[1])
-
-                resultados_list_heur.append((instancia, d, makespan, energy))
-            else:
-                print "[ERROR] cargando heuristica %s" % d
-                exit(-1)
-
-    for instancia in instancias:           
-        path = palsRuso_dir + '/palsRuso.' + instancia + '.metrics'
-        print path
+        path = minmin_dir + '/MinMin.scenario.' + instancia[0] + '.workload.' + instancia[1] + '.metrics'
+        #print path
         
         if os.path.isfile(path):
             metrics_file = open(path)
@@ -64,61 +32,81 @@ if __name__ == '__main__':
             makespan = float(values[0])
             energy = float(values[1])
 
-            resultados_palsRuso[instancia] = (makespan, energy)
+            resultados_minmin[instancia] = (makespan, energy)
         else:
-            print "[ERROR] cargando heuristica palsRuso"
+            print "[ERROR] cargando heuristica MinMin"
             exit(-1)
 
     for instancia in instancias:           
-        ejecucion = ejecuciones_pals[instancia][0]
+        path = pals_ruso_dir + '/pals-ruso.scenario.' + instancia[0] + '.workload.' + instancia[1] + '.metrics'
+        #print path
         
-        path = pals_dir + '/' + instancia + '/' + ejecucion + '.metrics'
-        print path
-
         if os.path.isfile(path):
             metrics_file = open(path)
-            
-            min_makespan = 0.0
-            min_energy = 0.0
-            
-            for line in metrics_file:
-                values = line.split(' ')
-                makespan = float(values[0])
-                energy = float(values[1])
-                
-                if min_makespan == 0.0: min_makespan = makespan
-                elif min_makespan > makespan: min_makespan = makespan
-                
-                if min_energy == 0.0: min_energy = energy
-                elif min_energy > energy: min_energy = energy
+            values = metrics_file.readline().split(' ')
+            makespan = float(values[0])
+            energy = float(values[1])
 
-            resultados_pals[instancia] = (min_makespan, min_energy)
+            resultados_pals_ruso[instancia] = (makespan, energy)
         else:
-            print "[ERROR] cargando heuristica pals"
+            print "[ERROR] cargando heuristica PALS del Ruso"
             exit(-1)
+
+    for instancia in instancias: 
+        abs_min_makespan = 0.0
+        abs_min_energy = 0.0
+        
+        aux_iter_metrics = []
+    
+        for iter in range(cant_iters):                      
+            dir_path = pals_dir + '/scenario.' + instancia[0] + '.workload.' + instancia[1] + '.' + str(iter) + '/'
+            file_name = 'pals-1.scenario.' + instancia[0] + '.workload.' + instancia[1] + '.metrics'
+            
+            path = dir_path + file_name 
+            #print path
+
+            if os.path.isfile(path):
+                metrics_file = open(path)
+                
+                min_makespan = 0.0
+                min_energy = 0.0
+                
+                for line in metrics_file:
+                    values = line.split(' ')
+                    makespan = float(values[0])
+                    energy = float(values[1])
+                    
+                    if min_makespan == 0.0: min_makespan = makespan
+                    elif min_makespan > makespan: min_makespan = makespan
+                    
+                    if min_energy == 0.0: min_energy = energy
+                    elif min_energy > energy: min_energy = energy
+
+                if abs_min_makespan == 0.0: abs_min_makespan = min_makespan
+                elif abs_min_makespan > min_makespan: abs_min_makespan = min_makespan
+                
+                if abs_min_energy == 0.0: abs_min_energy = min_energy
+                elif abs_min_energy > min_energy: abs_min_energy = min_energy
+
+                aux_iter_metrics.append((min_makespan, min_energy))
+            else:
+                print "[ERROR] cargando heuristica pals"
+                exit(-1)
+                
+        resultados_pals[instancia] = (abs_min_makespan, abs_min_energy)
             
     print "====== Tabla de makespan ======"
     print "Instancia,MinMin,PALS Ruso,PALS Ruso vs MinMin,PALS 2obj,PALS 2obj vs MinMin"
     for instancia in instancias:
-        makespan_minmin = 0.0
-    
-        for (r_instancia, r_d, r_makespan, r_energy) in resultados_list_heur:
-            if instancia == r_instancia and r_d == 'MinMin':
-                makespan_minmin = r_makespan
-
-        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % (instancia, makespan_minmin, \
-            resultados_palsRuso[instancia][0], 100.0 - (resultados_palsRuso[instancia][0] * 100.0 / makespan_minmin), \
-            resultados_pals[instancia][0], 100.0 - (resultados_pals[instancia][0] * 100.0 / makespan_minmin))
+        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % ('s' + instancia[0] + ' ' + instancia[1], \
+            resultados_minmin[instancia][0], \
+            resultados_pals_ruso[instancia][0], 100.0 - (resultados_pals_ruso[instancia][0] * 100.0 / resultados_minmin[instancia][0]), \
+            resultados_pals[instancia][0], 100.0 - (resultados_pals[instancia][0] * 100.0 / resultados_minmin[instancia][0]))
 
     print "====== Tabla de energía ======"
     print "Instancia,MinMin,PALS Ruso,PALS Ruso vs MinMin,PALS 2obj,PALS 2obj vs MinMin"
     for instancia in instancias:
-        energy_minmin = 0.0
-    
-        for (r_instancia, r_d, r_makespan, r_energy) in resultados_list_heur:
-            if instancia == r_instancia and r_d == 'MinMin':
-                energy_minmin = r_energy
-
-        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % (instancia, energy_minmin, \
-            resultados_palsRuso[instancia][1], 100.0 - (resultados_palsRuso[instancia][1] * 100.0 / energy_minmin), \
-            resultados_pals[instancia][1], 100.0 - (resultados_pals[instancia][1] * 100.0 / energy_minmin))
+        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % ('s' + instancia[0] + ' ' + instancia[1], \
+            resultados_minmin[instancia][1], \
+            resultados_pals_ruso[instancia][1], 100.0 - (resultados_pals_ruso[instancia][1] * 100.0 / resultados_minmin[instancia][1]), \
+            resultados_pals[instancia][1], 100.0 - (resultados_pals[instancia][1] * 100.0 / resultados_minmin[instancia][1]))
