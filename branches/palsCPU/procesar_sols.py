d9b505f9f897feff5a9b2e40c6d4a9a6ab648d44
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-02-02 02:18:10 +0000

    scripts

--- branches/palsCPU/procesar_sols.py
+++ branches/palsCPU/procesar_sols.py
@@ -71,7 +71,11 @@ if __name__ == '__main__':
     for instancia in instancias: 
         abs_min_makespan = 0.0
         abs_min_energy = 0.0
-        
+    
+        total_makespan = 0.0
+        total_energy = 0.0
+        total_sols = 0
+    
         aux_iter_metrics = []
     
         for iter in range(cant_iters):                      
@@ -83,7 +87,8 @@ if __name__ == '__main__':
 
             if os.path.isfile(path):
                 metrics_file = open(path)
-                
+               
+                sols = 0 
                 min_makespan = 0.0
                 min_energy = 0.0
                 
@@ -91,7 +96,8 @@ if __name__ == '__main__':
                     values = line.split(' ')
                     makespan = float(values[0])
                     energy = float(values[1])
-                    
+                    sols = sols + 1
+
                     if min_makespan == 0.0: min_makespan = makespan
                     elif min_makespan > makespan: min_makespan = makespan
                     
@@ -104,25 +110,52 @@ if __name__ == '__main__':
                 if abs_min_energy == 0.0: abs_min_energy = min_energy
                 elif abs_min_energy > min_energy: abs_min_energy = min_energy
 
-                aux_iter_metrics.append((min_makespan, min_energy))
+                aux_iter_metrics.append((sols, min_makespan, min_energy))
+
+                total_makespan = total_makespan + min_makespan
+                total_energy = total_energy + min_energy
+                total_sols = total_sols + sols
             else:
                 print "[ERROR] cargando heuristica pals"
                 exit(-1)
-                
-        resultados_pals[instancia] = (abs_min_makespan, abs_min_energy)
-            
+ 
+        avg_makespan = total_makespan/cant_iters
+        avg_energy = total_energy/cant_iters
+
+        aux_stdev_makespan = 0.0
+        aux_stdev_energy = 0.0
+
+        for (cant_s, min_m, min_e) in aux_iter_metrics:
+            aux_stdev_makespan = aux_stdev_makespan + math.pow(min_m - avg_makespan, 2)
+            aux_stdev_energy = aux_stdev_energy + math.pow(min_e - avg_energy, 2)
+
+        stdev_makespan = math.sqrt((1.0/(cant_iters-1.0))*aux_stdev_makespan)
+        stdev_energy = math.sqrt((1.0/(cant_iters-1.0))*aux_stdev_energy)
+
+        resultados_pals[instancia] = (abs_min_makespan, abs_min_energy, total_sols/cant_iters, avg_makespan, stdev_makespan, avg_energy, stdev_energy)        
+ 
     print "====== Tabla de makespan ======"
-    print "Instancia,MinMin,PALS Ruso,PALS Ruso vs MinMin,PALS 2obj,PALS 2obj vs MinMin"
+    print "Instancia,MinMin,PALS Ruso,PALS Ruso vs MinMin,PALS 2obj,PALS 2obj vs MinMin, Avg PALS 2obj, Stdev PALS 2obj, Avg ND"
     for instancia in instancias:
-        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % ('s' + instancia[0] + ' ' + instancia[1], \
+        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%s" % ('s' + instancia[0] + ' ' + instancia[1], \
             resultados_minmin[instancia][0], \
-            resultados_pals_ruso[instancia][0], 100.0 - (resultados_pals_ruso[instancia][0] * 100.0 / resultados_minmin[instancia][0]), \
-            resultados_pals[instancia][0], 100.0 - (resultados_pals[instancia][0] * 100.0 / resultados_minmin[instancia][0]))
+            resultados_pals_ruso[instancia][0], \
+            100.0 - (resultados_pals_ruso[instancia][0] * 100.0 / resultados_minmin[instancia][0]), \
+            resultados_pals[instancia][0], \
+            100.0 - (resultados_pals[instancia][0] * 100.0 / resultados_minmin[instancia][0]), \
+            resultados_pals[instancia][3], \
+            resultados_pals[instancia][4] * 100.0 / resultados_pals[instancia][3], \
+            resultados_pals[instancia][2])
 
     print "====== Tabla de energía ======"
     print "Instancia,MinMin,PALS Ruso,PALS Ruso vs MinMin,PALS 2obj,PALS 2obj vs MinMin"
     for instancia in instancias:
-        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f" % ('s' + instancia[0] + ' ' + instancia[1], \
+        print "%s,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%.1f,%s" % ('s' + instancia[0] + ' ' + instancia[1], \
             resultados_minmin[instancia][1], \
-            resultados_pals_ruso[instancia][1], 100.0 - (resultados_pals_ruso[instancia][1] * 100.0 / resultados_minmin[instancia][1]), \
-            resultados_pals[instancia][1], 100.0 - (resultados_pals[instancia][1] * 100.0 / resultados_minmin[instancia][1]))
+            resultados_pals_ruso[instancia][1], \
+            100.0 - (resultados_pals_ruso[instancia][1] * 100.0 / resultados_minmin[instancia][1]), \
+            resultados_pals[instancia][1], \
+            100.0 - (resultados_pals[instancia][1] * 100.0 / resultados_minmin[instancia][1]), \
+            resultados_pals[instancia][5], \
+            resultados_pals[instancia][6] * 100.0 / resultados_pals[instancia][5], \
+            resultados_pals[instancia][2])
