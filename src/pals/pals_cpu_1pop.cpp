48bf8a00aabe2dccd226704bb707f379c3f40ff5
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-02-05 17:09:21 +0000

    revert fix iters

--- src/pals/pals_cpu_1pop.cpp
+++ src/pals/pals_cpu_1pop.cpp
@@ -896,8 +896,10 @@ void* pals_cpu_1pop_thread(void *thread_arg)
                 }
 
                 int work_do_iteration = 1;
-                int work_iteration_size = (int)floor((PALS_CPU_1POP_WORK__THREAD_ITERATIONS / 2) + 
-                    (random * (PALS_CPU_1POP_WORK__THREAD_ITERATIONS / 2)));
+                /*int work_iteration_size = (int)floor((PALS_CPU_1POP_WORK__THREAD_ITERATIONS / 2) + 
+                    (random * (PALS_CPU_1POP_WORK__THREAD_ITERATIONS / 2)));*/
+
+                int work_iteration_size = (int)floor(random * PALS_CPU_1POP_WORK__THREAD_ITERATIONS);
 
                 while (work_do_iteration == 1) {
                     work_do_iteration = 0;
