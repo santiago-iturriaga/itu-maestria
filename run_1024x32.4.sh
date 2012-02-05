85fd08fe014f6d627803b83d1618448c0d0ea672
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-02-05 03:02:01 +0000

    Ajuste scripts para speedup

--- run_1024x32.4.sh
+++ run_1024x32.4.sh
@@ -3,6 +3,8 @@ INSTANCES_PATH="instancias/1024x32.ME"
 SOLUTIONS_BASE_DIR="1024x32.4"
 THREADS=4
 ITERATIONS=15
+PALS_ITERATIONS=2500000
+PALS_TIMEOUT=100
 
 VERIFICADOR="bin/verificador"
 MINMIN_METRICS_PATH="list-heuristics/1024x32/MinMin"
@@ -12,17 +14,42 @@ ALGORITHMS[0]="bin/pals_cpu"
 ALGORITHMS_ID[0]=1
 ALGORITHMS_OUTNAME[0]="pals-1"
 
-SCENARIOS[0]=10
-SCENARIOS[1]=13
-SCENARIOS[2]=16
-SCENARIOS[3]=19
+SCENARIOS[0]=0
+SCENARIOS[1]=3
+SCENARIOS[2]=6
+SCENARIOS[3]=9
+SCENARIOS[4]=10
+SCENARIOS[5]=11
+SCENARIOS[6]=13
+SCENARIOS[7]=14
+SCENARIOS[8]=16
+SCENARIOS[9]=17
+SCENARIOS[10]=19
 
 WORKLOADS[0]="A.u_c_hihi"
-WORKLOADS[1]="A.u_c_lolo"
-WORKLOADS[2]="A.u_i_hihi"
-WORKLOADS[3]="A.u_i_lolo"
-WORKLOADS[4]="A.u_s_hihi"
-WORKLOADS[5]="A.u_s_lolo"
+WORKLOADS[1]="A.u_c_hilo"
+WORKLOADS[2]="A.u_c_lohi"
+WORKLOADS[3]="A.u_c_lolo"
+WORKLOADS[4]="A.u_i_hihi"
+WORKLOADS[5]="A.u_i_hilo"
+WORKLOADS[6]="A.u_i_lohi"
+WORKLOADS[7]="A.u_i_lolo"
+WORKLOADS[8]="A.u_s_hihi"
+WORKLOADS[9]="A.u_s_hilo"
+WORKLOADS[10]="A.u_s_lohi"
+WORKLOADS[11]="A.u_s_lolo"
+WORKLOADS[12]="B.u_c_hihi"
+WORKLOADS[13]="B.u_c_hilo"
+WORKLOADS[14]="B.u_c_lohi"
+WORKLOADS[15]="B.u_c_lolo"
+WORKLOADS[16]="B.u_i_hihi"
+WORKLOADS[17]="B.u_i_hilo"
+WORKLOADS[18]="B.u_i_lohi"
+WORKLOADS[19]="B.u_i_lolo"
+WORKLOADS[20]="B.u_s_hihi"
+WORKLOADS[21]="B.u_s_hilo"
+WORKLOADS[22]="B.u_s_lohi"
+WORKLOADS[23]="B.u_s_lolo"
 
 #mkdir -p ${SOLUTIONS_DIR}
 #rm ${SOLUTIONS_DIR}/*.metrics
@@ -45,7 +72,7 @@ do
                 rm ${OUT}.*
             
                 RAND=$RANDOM
-                EXEC="${ALGORITHMS[a]} ${INSTANCES_PATH}/scenario.${SCENARIOS[s]} ${INSTANCES_PATH}/workload.${WORKLOADS[w]} ${DIMENSIONS} ${ALGORITHMS_ID[a]} ${THREADS} ${RAND}"
+                EXEC="${ALGORITHMS[a]} ${INSTANCES_PATH}/scenario.${SCENARIOS[s]} ${INSTANCES_PATH}/workload.${WORKLOADS[w]} ${DIMENSIONS} ${ALGORITHMS_ID[a]} ${THREADS} ${RAND} ${PALS_TIMEOUT} ${PALS_ITERATIONS}"
                 echo ${EXEC}
                 time (${EXEC} >> ${OUT}.sols 2> ${OUT}.info) 2> ${OUT}.time
             
