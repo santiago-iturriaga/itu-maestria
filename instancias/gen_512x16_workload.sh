ba7c34e0c7bf47f45f3ea966167e4f857b82d19f
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-02-14 02:43:32 +0000

    final

--- instancias/gen_512x16_workload.sh
+++ instancias/gen_512x16_workload.sh
@@ -11,7 +11,7 @@ i=0
 cp ${ME_INSTANCES_PATH}/scenario.* ${SOLUTIONS_DIR}
 
 cd ${M_INSTANCES_PATH}
-files=$(ls *)
+files=$(ls B.*)
 cd ${BASE}
 
 for workload_file in ${files}
@@ -24,3 +24,19 @@ do
     
     i=$((i+1))
 done
+
+cd ${M_INSTANCES_PATH}
+files=$(ls A.*)
+cd ${BASE}
+
+for workload_file in ${files}
+do
+    echo "${workload_file} to workload.${i}"
+
+    (python remove_first_line.py ${M_INSTANCES_PATH}/${workload_file}) > ${SOLUTIONS_DIR}/workload.${workload_file}
+    #echo ${EXEC}
+    #${EXEC}
+
+    i=$((i+1))
+done
+
