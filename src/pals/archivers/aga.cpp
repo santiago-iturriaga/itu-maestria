8fa5dcb0a99fa0d6d13f01bea56308327ef9fd4a
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-16 08:15:39 +0000

    obj genericos

--- src/pals/archivers/aga.cpp
+++ src/pals/archivers/aga.cpp
@@ -7,219 +7,265 @@
 
 #include "adhoc.h"
 
-int archivers_aga(struct pals_cpu_1pop_thread_arg *instance, int new_solution_pos)
+/*
+void archive_soln(sol *s)
 {
-    if (DEBUG_DEV) fprintf(stdout, "========================================================\n");
-    double random = 0.0;
+    // given a solution s, add it to the archive if
+    // a) the archive is empty
+    // b) the archive is not full and s is not dominated or equal to anything currently in the archive
+    // c) s dominates anything in the archive
+    // d) the archive is full but s is nondominated and is in a no more crowded square than at least one solution
+    // in addition, maintain the archive such that all solutions are nondominated.
 
-    float makespan_new, energy_new;
-    makespan_new = get_makespan(&(instance->population[new_solution_pos]));
-    energy_new = get_energy(&(instance->population[new_solution_pos]));
+    int i;
+    int repl;
+    int yes = 0;
+    int most;
+    int result;
+    int join = 0;
+    int old_arclength;
+    int set = 0;
 
-    if (*(instance->best_energy_solution) == -1) *(instance->best_energy_solution) = new_solution_pos;
-    if (*(instance->best_makespan_solution) == -1) *(instance->best_makespan_solution) = new_solution_pos;
-
-    float best_energy_value = get_energy(&(instance->population[*(instance->best_energy_solution)]));
-    float best_makespan_value = get_makespan(&(instance->population[*(instance->best_makespan_solution)]));
-
-    if (DEBUG_DEV)
+    int tag[MAX_ARC];
+    sol *tmp;
+    if (!(tmp = (sol *)malloc(MAX_ARC*sizeof(sol))))
     {
-        fprintf(stdout, "[DEBUG] Population\n");
-        fprintf(stdout, "        Population_count: %d\n", *(instance->population_count));
-        fprintf(stdout, "        Solution to eval: %d\n", new_solution_pos);
-        fprintf(stdout, "        Makespan        : %f\n", makespan_new);
-        fprintf(stdout, "        Energy          : %f\n", energy_new);
-        fprintf(stdout, "        Best makespan   : %f (%d)\n", best_makespan_value, *(instance->best_makespan_solution));
-        fprintf(stdout, "        Best energy     : %f (%d)\n", best_energy_value, *(instance->best_energy_solution));
+        printf("Out of memory\n");
+        exit(-1);
+    }
 
-        for (int i = 0; i < instance->population_max_size; i++)
+    for (i = 0; i < archive; i++)
+    {
+        tag[i]=0;
+    }
+
+    if (arclength == 0)
+    {
+        add_to_archive(s);
+        return;
+    }
+
+    i = 0;
+    result = 0;
+    while((i < arclength)&&(result!=-1))
+    {
+        result = equal(s->obj, (&arc[i])->obj, objectives);
+        if (result == 1)
+            break;
+        //MINIMIZE MAXIMIZE
+        if (minmax==0)
+            result = compare_min(s->obj, (&arc[i])->obj, objectives);
+        else
+            result = compare_max(s->obj, (&arc[i])->obj, objectives);
+        //  printf("%d\n", result);
+
+        if ((result == 1)&&(join == 0))
         {
-            float makespan, energy;
-            makespan = 0;
-            energy = 0;
-            if (instance->population[i].status == 2) {
-                makespan = get_makespan(&(instance->population[i]));
-                energy = get_energy(&(instance->population[i]));
-            }
+            arc[i] = *s;
+            join = 1;
+        }
+        else if (result == 1)
+        {
+            tag[i]=1;
+            set = 1;
+        }
+        i++;
+    }
 
-            fprintf(stdout, " >> sol.pos[%d] init=%d status=%d makespan=%f energy=%f\n", i,
-                instance->population[i].initialized, instance->population[i].status,
-                makespan, energy);
+    old_arclength = arclength;
+    if (set==1)
+    {
+        for (i = 0; i < arclength; i++)
+        {
+            tmp[i] = arc[i];
+        }
+        arclength = 0;
+
+        for (i = 0; i < old_arclength; i++)
+        {
+            if (tag[i]!=1)
+            {
+                arc[arclength]=tmp[i];
+                arclength++;
+            }
         }
     }
 
-    int candidato_reemplazo = -1;
-    int solutions_deleted = 0;
-    int new_solution_is_dominated = 0;
-
-    int s_idx = -1;
-    for (int s_pos = 0; (s_pos < instance->population_max_size) && (new_solution_is_dominated == 0); s_pos++)
+    if ((join==0)&&(result==0))  // ie solution is non-dominated by the list
     {
-
-        if ((instance->population[s_pos].status > SOLUTION__STATUS_EMPTY) &&
-            (instance->population[s_pos].initialized == 1) &&
-            (s_pos != new_solution_pos))
+        if (arclength == archive)
         {
-            s_idx++;
-
-            // Calculo no dominancia del elemento nuevo con el actual.
-            float makespan, energy;
-            makespan = get_makespan(&(instance->population[s_pos]));
-            energy = get_energy(&(instance->population[s_pos]));
-
-            if (DEBUG_DEV) fprintf(stdout, "[%d] Makespan: %f %f || Energy %f %f\n", s_pos, makespan, makespan_new, energy, energy_new);
-
-            if ((makespan <= makespan_new) && (energy <= energy_new))
+            most = grid_pop[s->grid_loc];
+            for (i = 0; i < arclength; i++)
             {
-                // La nueva solucion es dominada por una ya existente.
-                new_solution_is_dominated = 1;
-
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] Individual %d is dominated by %d\n", new_solution_pos, s_pos);
-            }
-            else if ((makespan_new <= makespan) && (energy_new <= energy))
-            {
-                // La nueva solucin domina a una ya existente.
-                solutions_deleted++;
-                instance->population_count[0] = instance->population_count[0] - 1;
-                instance->population[s_pos].status = SOLUTION__STATUS_EMPTY;
-
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] Removed individual %d because %d is better\n", s_pos, new_solution_pos);
-            }
-            else
-            {
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] No definido\n");
-
-                if ((instance->population_count[0] + instance->count_threads) >= instance->population_max_size) {
-                    // Ninguna de las dos soluciones es dominada por la otra.
-
-                    if ((*(instance->best_energy_solution) == s_pos) && (best_energy_value < energy_new)) {
-                        // No lo puedo reemplazar porque es el mejor energy.
-
-                    } else if ((*(instance->best_makespan_solution) == s_pos) && (best_makespan_value < makespan_new)) {
-                        // No lo puedo reemplazar porque es el mejor makespan.
-
-                    } else {
-                        if (candidato_reemplazo == -1) {
-                            candidato_reemplazo = s_pos;
-                        } else {
-                            float diff_makespan_candidato_actual;
-                            float diff_energy_candidato_actual;
-                            diff_makespan_candidato_actual = get_makespan(&(instance->population[candidato_reemplazo])) - makespan_new;
-                            diff_energy_candidato_actual = get_energy(&(instance->population[candidato_reemplazo])) - energy_new;
-
-                            float diff_makespan_individuo_actual;
-                            float diff_energy_individuo_actual;
-                            diff_makespan_individuo_actual = makespan - makespan_new;
-                            diff_energy_individuo_actual = makespan - energy_new;
-
-                            if (DEBUG_DEV) {
-                                fprintf(stdout, "[ND] Evaluo candidato contra:\n");
-                                fprintf(stdout, "[DEBUG] Makespan vs: %f vs %f (%f , %f)\n", get_makespan(&(instance->population[candidato_reemplazo])),
-                                    get_makespan(&(instance->population[s_pos])),diff_makespan_candidato_actual, diff_makespan_individuo_actual);
-                                fprintf(stdout, "[DEBUG] Energy vs: %f vs %f (%f , %f)\n", get_energy(&(instance->population[candidato_reemplazo])),
-                                    get_energy(&(instance->population[s_pos])),diff_energy_candidato_actual, diff_energy_individuo_actual);
-                            }
-
-                            if (diff_makespan_individuo_actual > diff_makespan_candidato_actual) {
-                                candidato_reemplazo = s_pos;
-
-                            } else if ((diff_makespan_individuo_actual == diff_makespan_candidato_actual) &&
-                                (diff_energy_individuo_actual > diff_energy_candidato_actual)) {
-
-                                candidato_reemplazo = s_pos;
-
-                            } else if ((diff_makespan_individuo_actual == diff_makespan_candidato_actual) &&
-                                (diff_energy_individuo_actual == diff_energy_candidato_actual)) {
-
-                                if (DEBUG_DEV) fprintf(stdout, "[ND] Sorteo un candidato.\n");
-
-                                #ifdef CPU_MERSENNE_TWISTER
-                                random = cpu_mt_generate(*(instance->thread_random_state));
-                                #endif
-                                #ifdef CPU_RAND
-                                random = cpu_rand_generate(*(instance->thread_random_state));
-                                #endif
-                                #ifdef CPU_DRAND48
-                                random = cpu_drand48_generate(*(instance->thread_random_state));
-                                #endif
-
-                                if (random > 0.5) {
-                                    candidato_reemplazo = s_pos;
-                                }
-                            }
-                        }
-                    }
+                if (grid_pop[(&arc[i])->grid_loc] > most)
+                {
+                    most = grid_pop[(&arc[i])->grid_loc];
+                    repl = i;
+                    yes = 1;
+                    //   printf("i = %d\n", i);
                 }
             }
-        }
-    }
-
-    if (new_solution_is_dominated == 0)
-    {
-        if ((instance->population_count[0] + instance->count_threads) < instance->population_max_size)
-        {
-            instance->population[new_solution_pos].status = SOLUTION__STATUS_READY;
-            instance->population_count[0] = instance->population_count[0] + 1;
-
-            if (energy_new < best_energy_value) {
-                *(instance->best_energy_solution) = new_solution_pos;
-
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] New best energy solution %d\n", new_solution_pos);
+            if (yes)
+            {
+                arc[repl] = *s;
             }
-            if (makespan_new < best_makespan_value) {
-                *(instance->best_makespan_solution) = new_solution_pos;
-
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] New best makespan solution %d\n", new_solution_pos);
-            }
-
-            if (DEBUG_DEV) fprintf(stdout, "[DEBUG] Added invidiual %d because is ND\n", new_solution_pos);
-            return 1;
         }
         else
         {
-            if (candidato_reemplazo != -1) {
-                if (DEBUG_DEV) {
-                    fprintf(stdout, "[DEBUG] Reemplazo por el individuo %d\n", candidato_reemplazo);
-                    fprintf(stdout, "[DEBUG] Makespan vs: %f vs %f\n", get_makespan(&(instance->population[candidato_reemplazo])),
-                        get_makespan(&(instance->population[new_solution_pos])));
-                    fprintf(stdout, "[DEBUG] Energy vs: %f vs %f\n", get_energy(&(instance->population[candidato_reemplazo])),
-                        get_energy(&(instance->population[new_solution_pos])));
-                }
-
-                instance->population[new_solution_pos].status = SOLUTION__STATUS_READY;
-                instance->population[candidato_reemplazo].status = SOLUTION__STATUS_EMPTY;
-
-                if (energy_new < best_energy_value) {
-                    *(instance->best_energy_solution) = new_solution_pos;
-
-                    if (DEBUG_DEV) fprintf(stdout, "[DEBUG] New best energy solution %d\n", new_solution_pos);
-                } /*else {
-                    assert(candidato_reemplazo != *(instance->best_energy_solution));
-                }*/
-                if (makespan_new < best_makespan_value) {
-                    *(instance->best_makespan_solution) = new_solution_pos;
-
-                    if (DEBUG_DEV) fprintf(stdout, "[DEBUG] New best makespan solution %d\n", new_solution_pos);
-                } /*else {
-                    assert(candidato_reemplazo != *(instance->best_makespan_solution));
-                }*/
-
-                return 1;
-            } else {
-                instance->population[new_solution_pos].status = SOLUTION__STATUS_EMPTY;
-                instance->total_population_full++;
-
-                if (DEBUG_DEV) fprintf(stdout, "[DEBUG] Discarded invidiual %d because there is no space left (threads=%d, count=%d, max=%d)\n",
-                        new_solution_pos, instance->count_threads, instance->population_count[0], instance->population_max_size);
-                return -1;
-            }
+            add_to_archive(s);
         }
     }
-    else
-    {
-        instance->population[new_solution_pos].status = SOLUTION__STATUS_EMPTY;
+    free(tmp);
+}
+*/
 
-        if (DEBUG_DEV) fprintf(stdout, "[DEBUG] Discarded invidiual %d because is dominated\n", new_solution_pos);
-        return 0;
+int find_loc(double *eval)
+{
+    /*
+    // find the grid location of a solution given a vector of its objective values
+
+    int loc = 0;
+    int d;
+    int n = 1;
+
+    int i;
+
+    int inc[MAX_OBJ];
+    double width[MAX_OBJ];
+
+    // printf("obj = %d, depth = %d\n", objectives, depth);
+
+    // if the solution is out of range on any objective, return 1 more than the maximum possible grid location number
+    for (i = 0; i < objectives; i++)
+    {
+        if ((eval[i] < gl_offset[i])||(eval[i] > gl_offset[i] + gl_range[i]))
+            return((int)pow(2,(objectives*depth)));
     }
+
+    for (i = 0; i < objectives; i++)
+    {
+        inc[i] = n;
+        n *=2;
+        width[i] = gl_range[i];
+    }
+
+    for (d = 1; d <= depth; d++)
+    {
+        for (i = 0; i < objectives; i++)
+        {
+            if(eval[i] < width[i]/2+gl_offset[i])
+                loc += inc[i];
+            else
+                gl_offset[i] += width[i]/2;
+        }
+        for (i = 0; i < objectives; i++)
+        {
+            inc[i] *= (objectives *2);
+            width[i] /= 2;
+        }
+    }
+    return(loc);
+    * */
+}
+
+#define OBJECTIVES 2
+
+#define MAX_GENES 1000          // change as necessary
+#define MAX_POP 10              // change as necessary
+#define MAX_ARC 200             // change as necessary
+#define MAX_LOC 32768           // number of locations in grid (set for a three-objective problem using depth 5)
+#define LARGE 2000000000        // should be about the maximum size of an integer for your compiler
+
+/* sol *s
+void update_grid(struct pals_cpu_1pop_thread_arg *instance, int new_solution_pos)
+{
+    // recalculate ranges for grid in the light of a new solution s
+    double offset[OBJECTIVES];
+    double largest[OBJECTIVES];
+
+    for (a = 0; a < OBJECTIVES; a++)
+    {
+        offset[a] = LARGE;
+        largest[a] = -LARGE;
+    }
+
+    for (b = 0; b < OBJECTIVES; b++)
+    {
+        for (a = 0; a < *(instance->population_count); a++)
+        {
+            if ((instance->population[a])->obj[b] < offset[b])
+                offset[b] = (&arc[a])->obj[b];
+                
+            if ((&arc[a])->obj[b] > largest[b])
+                largest[b] = (&arc[a])->obj[b];
+        }
+    }
+    //    printf("oldCURENT:largest = %f, offset = %f\n", largest[0], offset[0]);
+    //    printf("oldCURENT:largest = %f, offset = %f\n", largest[1], offset[1]);
+
+    static int change = 0;
+    int a, b;
+    int square;
+    double sse;
+    double product;
+
+    for (b = 0; b < objectives; b++)
+    {
+        if (s->obj[b] < offset[b])
+            offset[b] = s->obj[b];
+        if (s->obj[b] > largest[b])
+            largest[b] = s->obj[b];
+    }
+
+    sse = 0;
+    product = 1;
+
+    for (a = 0; a < objectives; a++)
+    {
+
+        sse += ((gl_offset[a] - offset[a])*(gl_offset[a] - offset[a]));
+        sse += ((gl_largest[a] - largest[a])*(gl_largest[a] - largest[a]));
+        product *= gl_range[a];
+    }
+
+    // printf("sse = %f\n", sse);
+                                 //if the summed squared error (difference) between old and new
+    if (sse > (0.1 * product * product))
+                                 //minima and maxima in each of the objectives
+    {                            //is bigger than 10 percent of the square of the size of the space
+        change++;                // then renormalise the space and recalculte grid locations
+
+        for (a = 0; a < objectives; a++)
+        {
+            gl_largest[a] = largest[a]+0.2*largest[a];
+            gl_offset[a] = offset[a]+0.2*offset[a];
+            gl_range[a] = gl_largest[a] - gl_offset[a];
+        }
+
+        for (a = 0; a < pow(2, (objectives*depth)); a++)
+        {
+            grid_pop[a] = 0;
+        }
+
+        for (a = 0; a < arclength; a++)
+        {
+            square = find_loc((&arc[a])->obj);
+            (&arc[a])->grid_loc = square;
+            grid_pop[square]++;
+
+        }
+    }
+    square = find_loc(s->obj);
+    s->grid_loc = square;
+    grid_pop[(int)pow(2,(objectives*depth))] = -5;
+    grid_pop[square]++;
+
+}*/
+
+int archivers_aga(struct pals_cpu_1pop_thread_arg *instance, int new_solution_pos)
+{
+    //update_grid(instance, new_solution_pos);  //calculate grid location of mutant solution and renormalize archive if necessary
+    //archive_soln(instance, new_solution_pos); //update the archive by removing all dominated individuals
+    return 0;
 }
