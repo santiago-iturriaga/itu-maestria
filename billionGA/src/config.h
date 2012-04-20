d15a9bb9a3043ceb93d2f6e180972ce06cb6f766
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-20 08:21:55 +0000

    bga init

--- billionGA/src/config.h
+++ billionGA/src/config.h
@@ -2,6 +2,6 @@
 #define DEBUG   1
 
 #define NUMBER_OF_SAMPLES       2
-#define MAX_PROB_VECTOR_BITS    900000000
+#define MAX_PROB_VECTOR_BITS    899999744
 
 #define RNUMBERS_PER_GEN        10000000
