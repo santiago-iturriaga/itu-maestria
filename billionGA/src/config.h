1d66bf1bf5a608e08984a6aa08e3a7a6f271ff3c
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-21 13:50:16 +0000

    model sampling

--- billionGA/src/config.h
+++ billionGA/src/config.h
@@ -2,7 +2,10 @@
 #define DEBUG   1
 
 #define NUMBER_OF_SAMPLES       2
+
 #define MAX_PROB_VECTOR_BITS    899999744
+// Debe ser divisible entre 32 (8 y 4)
+
 #define INIT_PROB_VECTOR_VALUE  0.5
 
 #define RNUMBERS_PER_GEN        10000000
--- billionGA/src/main.cu
+++ billionGA/src/main.cu
@@ -9,7 +9,7 @@
 #include "billionga.h"
 
 #define TEST_PROBLEM_SIZE 899999744
-// Debe ser divisible entre 512, 128, y 8???
+// Debe ser divisible entre 32 (8 y 4)... y 512, 128???
 
 inline int termination_criteria_met(struct bga_state *problem_state, int iteration_count) {
     return (iteration_count == 1);
