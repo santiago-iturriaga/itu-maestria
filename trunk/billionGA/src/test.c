34242aaeff1ae151cd2c961b59e15cd63505c796
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-21 17:18:36 +0000

    sampling

--- trunk/billionGA/src/test.c
+++ trunk/billionGA/src/test.c
@@ -3,6 +3,24 @@
 #include <limits.h>
 #include <string.h>
 
+int sum_bits_from_int(int data) {
+    unsigned int sum = 0;
+    unsigned int starting = 1 << ((sizeof(int) * 8)-1);
+    int shifts = 32;
+    
+    for (unsigned int z = starting; z > 0; z >>= 1)
+    {
+        shifts--;
+        
+        fprintf(stdout, "z = %u\n", z);
+        fprintf(stdout, "data & z = %u\n", data & z);
+        sum += (data & z) >> shifts;
+        fprintf(stdout, "(data & z) >> shifts = %u\n", (data & z) >> shifts);
+    }
+    
+    return sum;   
+}
+
 const char* int_to_binary(int x)
 {
     int b_size = sizeof(char) * ((sizeof(int) * 8) + 1);
@@ -12,11 +30,11 @@ const char* int_to_binary(int x)
 
     int pos = 0;
     unsigned int starting = 1 << ((sizeof(int) * 8)-1);
-    fprintf(stdout, "z(-1) = %d\n", starting);
+    //fprintf(stdout, "z(-1) = %d\n", starting);
     
     for (unsigned  int z = starting; z > 0; z >>= 1)
     {        
-        fprintf(stdout, "z = %d\n", z);
+        //fprintf(stdout, "z = %d\n", z);
         
         if ((x & z) == z) {
             b[pos] =  '1';
@@ -54,5 +72,6 @@ int main(int argc, char **argv) {
     
     for (int i = 0; i < 1; i++) {
         printf("%s (%d)\n", int_to_binary(((int*)current_block_sample)[0]), ((int*)current_block_sample)[0]);
+        printf("sum %d\n", sum_bits_from_int(((int*)current_block_sample)[0]));
     }
 }
