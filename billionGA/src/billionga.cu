ed26218f53a6f8eacce054138125143f245c8cb1
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-21 14:24:11 +0000

    sampling

--- billionGA/src/billionga.cu
+++ billionGA/src/billionga.cu
@@ -218,7 +218,7 @@ void bga_initialization(struct bga_state *state, long number_of_bits, int number
             size_t sample_vector_size = sizeof(int) * (current_prob_vector_number_of_bits >> 5);
             fprintf(stdout, "current_prob_vector_number_of_bits = %d\ncurrent_prob_vector_number_of_bits >> 5 = %d\ncurrent_prob_vector_number_of_bits >> 5 = %d\n",
                 current_prob_vector_number_of_bits, current_prob_vector_number_of_bits >> 5, current_prob_vector_number_of_bits & ((1<<5)-1));
-            assert(current_prob_vector_number_of_bits & ((1<<5)-1) != 0);
+            assert(current_prob_vector_number_of_bits & ((1<<5)-1) == 0);
 
             #ifdef INFO
             fprintf(stdout, "[INFO] > Requesting sample %d GPU memory for vector %d\n", sample_number, prob_vector_number);
