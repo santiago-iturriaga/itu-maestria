60db7db789effd767ca41d513f934e0412b8f4c2
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 19:35:59 +0000

    scripts

--- trunk/billionGA/run_clu.sh
+++ trunk/billionGA/run_clu.sh
@@ -3,4 +3,4 @@ export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusteruse
 cd /home/clusterusers/siturriaga/itu-maestria/trunk/billionGA/
 rm salida.txt
 make clu
-time (bin/billionga 1048576 1000 1048576 0 > salida.txt)
+time (bin/billionga 1048576 100000 1048576 0 > salida.txt)
