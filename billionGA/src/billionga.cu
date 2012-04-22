7733c7bdc3fc13eac923d815f28e6f41142a8292
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 13:41:13 +0000

    sampling

--- billionGA/src/billionga.cu
+++ billionGA/src/billionga.cu
@@ -383,7 +383,7 @@ __global__ void kern_sample_prob_vector(float *gpu_prob_vector, int prob_vector_
         sample_position = (samples_per_loop * loop) + (bid * blockDim.x) + tid;
         prob_vector_position = prob_vector_starting_pos + sample_position;
         
-        if (sample_position < max_samples_doable) {
+        //if (sample_position < max_samples_doable) {
             if (gpu_prob_vector[prob_vector_position]+2 >= prng_vector[sample_position]) {
                 // 1
                 atomicOr(&(current_block_sample[tid_int]), (1 << tid_bit));
@@ -391,7 +391,7 @@ __global__ void kern_sample_prob_vector(float *gpu_prob_vector, int prob_vector_
                 // 0
                 atomicAnd(&(current_block_sample[tid_int]), ~(1 << tid_bit));
             }            
-        }
+        //}
 
         __syncthreads();
                
