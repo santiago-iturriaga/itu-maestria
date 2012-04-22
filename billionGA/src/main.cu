43352c4a2cd2cffd5bec3178b739a39e16dd8930
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 12:55:17 +0000

    done sampling

--- billionGA/src/main.cu
+++ billionGA/src/main.cu
@@ -9,8 +9,8 @@
 #include "billionga.h"
 
 //#define TEST_PROBLEM_SIZE 32
-#define TEST_PROBLEM_SIZE 1024
-//#define TEST_PROBLEM_SIZE 899999744
+//#define TEST_PROBLEM_SIZE 1024
+#define TEST_PROBLEM_SIZE 899999744
 // Debe ser divisible entre 32 (8 y 4)... y 512, 128???
 
 inline int termination_criteria_met(struct bga_state *problem_state, int iteration_count) {
