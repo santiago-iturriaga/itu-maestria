7de3021ddd324069376fed0508a183dcb510c146
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 19:02:35 +0000

    scripts

--- billionGA/run_clu.sh
+++ billionGA/run_clu.sh
@@ -1,2 +1,3 @@
 cd /home/clusterusers/siturriaga/itu-maestria/trunk/billionGA/
+export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusterusers/siturriaga/cuda/lib
 time (bin/billionga 1048576 1000 1048576 0 > salida.txt)
