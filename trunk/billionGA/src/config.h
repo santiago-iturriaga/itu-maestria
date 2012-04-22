a3804b06d3b7c5aa9f2f35b90337b807c1034acb
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 16:36:12 +0000

    sampling done for sure

--- trunk/billionGA/src/config.h
+++ trunk/billionGA/src/config.h
@@ -3,7 +3,8 @@
 
 #define NUMBER_OF_SAMPLES       2
 
-#define MAX_PROB_VECTOR_BITS    899999744
+#define MAX_PROB_VECTOR_BITS    1048576
+// #define MAX_PROB_VECTOR_BITS    99999744
 // Debe ser divisible entre 32 (8 y 4)
 
 #define INIT_PROB_VECTOR_VALUE  0.5
@@ -12,7 +13,7 @@
 //#define RNUMBERS_PER_GEN        84
 //#define RNUMBERS_PER_GEN        168
 //#define RNUMBERS_PER_GEN        1000020
-//#define RNUMBERS_PER_GEN        1048576
-#define RNUMBERS_PER_GEN        67108864
+#define RNUMBERS_PER_GEN        1048576
+//#define RNUMBERS_PER_GEN        67108864
 //#define RNUMBERS_PER_GEN        10000032
 //#define RNUMBERS_PER_GEN        134217728
