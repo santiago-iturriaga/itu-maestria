6576947c64e20c1004be178183c1429367914d29
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 13:07:31 +0000

    done sampling

--- trunk/billionGA/src/config.h
+++ trunk/billionGA/src/config.h
@@ -8,6 +8,7 @@
 
 #define INIT_PROB_VECTOR_VALUE  0.5
 
-#define RNUMBERS_PER_GEN        84
+//#define RNUMBERS_PER_GEN        84
+#define RNUMBERS_PER_GEN        168
 //#define RNUMBERS_PER_GEN        10000032
 //#define RNUMBERS_PER_GEN        1000020
