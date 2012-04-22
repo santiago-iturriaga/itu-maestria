a7834de4784e5e3175bf93916deddd643d5de321
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-22 13:59:54 +0000

    sampling

--- billionGA/src/config.h
+++ billionGA/src/config.h
@@ -8,7 +8,8 @@
 
 #define INIT_PROB_VECTOR_VALUE  0.5
 
-#define RNUMBERS_PER_GEN        84
+#define RNUMBERS_PER_GEN        64
+//#define RNUMBERS_PER_GEN        84
 //#define RNUMBERS_PER_GEN        168
 //#define RNUMBERS_PER_GEN        10000032
 //#define RNUMBERS_PER_GEN        1000020
