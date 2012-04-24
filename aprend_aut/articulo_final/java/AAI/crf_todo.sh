5cad44aff6dfe9999deb9dccfc03ce1defca19ee
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-24 08:54:05 +0000

    Version funcionando (1)

--- aprend_aut/articulo_final/java/AAI/crf_todo.sh
+++
@@ -1,3 +0,0 @@
-./train_crf_prueba.sh
-./test_crf_prueba.sh > corpus/crf_2.result.txt 
-./comparar_resultado.sh corpus/test_full_2.txt corpus/crf_2.result.txt 
--- aprend_aut/articulo_final/java/AAI/test_crf_prueba4.sh
+++
@@ -1,2 +0,0 @@
-java -classpath /home/santiago/eclipse/java-workspace/AAI/bin:/home/santiago/eclipse/java-workspace/AAI/lib/mallet-deps.jar \
-	AII.CRFTest model_crf/svm_crf_2.model > model_crf/svm_result_2.txt	
