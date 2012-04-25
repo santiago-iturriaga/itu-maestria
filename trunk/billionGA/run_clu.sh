86151e6268ac9511287cdcc719d0908728f90dae
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-25 13:15:30 +0000

    scripts

--- trunk/billionGA/run_clu.sh
+++ trunk/billionGA/run_clu.sh
@@ -6,7 +6,7 @@ cd /home/clusterusers/siturriaga/itu-maestria/trunk/billionGA/
 #rm out-*.txt
 #make clu
 
-bin/billionga 1073741824 100000 536870912 0
+bin/billionga 1073741824 100000 268435456 0
 #bin/billionga 1048576 2000000 524288 0
 #bin/billionga 1048576 2000000 1048576 0
 #time (bin/billionga 1048576 2000000 1048576 0 0 > out-1m.txt)
