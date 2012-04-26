a101b6b6d2291b09ac5cd06b2bcf639ff71aad99
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-26 12:59:11 +0000

    Tope de 5000 iteraciones

--- palsGPU/src/config.h
+++ palsGPU/src/config.h
@@ -10,12 +10,10 @@
 //#define PALS_COUNT 5
 //#define PALS_COUNT 25
 //#define PALS_COUNT 250
-//#define PALS_COUNT 5000
+#define PALS_COUNT 5000
 //#define PALS_COUNT 10000
 //#define PALS_COUNT 50000
 //#define PALS_COUNT 100000
-#define PALS_COUNT 1000000
+//#define PALS_COUNT 1000000
 
 #endif //CONFIG_H_
-
-
