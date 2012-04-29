6fe63291e90b2e678746d2165eccbc78a81866fc
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-04-29 10:39:34 +0000

    cambios cluster lu

--- trunk/palsGPU/src/pals/pals_gpu_rtask.cu
+++ trunk/palsGPU/src/pals/pals_gpu_rtask.cu
@@ -668,8 +668,10 @@ void pals_gpu_rtask(struct params &input, struct matrix *etc_matrix, struct solu
     clock_gettime(CLOCK_REALTIME, &ts_stop_condition_current);
 
     int iter;
-    for (iter = 0; (iter < PALS_COUNT) && (convergence_flag == 0)
-        && (ts_stop_condition_current.tv_sec - ts_stop_condition_start.tv_sec) <= 5; iter++) {
+    /*for (iter = 0; (iter < PALS_COUNT) && (convergence_flag == 0)
+        && (ts_stop_condition_current.tv_sec - ts_stop_condition_start.tv_sec) <= 5; iter++) {*/
+    for (iter = 0; (iter < PALS_COUNT); iter++) {
+        
         if (DEBUG) fprintf(stdout, "[INFO] Iteracion %d =====================\n", iter);
 
         // ==============================================================================
