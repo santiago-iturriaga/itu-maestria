b44e43c6af2883a67088146f8ab752022ded2929
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-05-02 13:51:48 +0000

    cambio de representacion en gpu

--- trunk/palsGPU/src/config.h
+++ trunk/palsGPU/src/config.h
@@ -7,13 +7,13 @@
 
 //#define PALS_COUNT 1
 //#define PALS_COUNT 2
-#define PALS_COUNT 10
+//#define PALS_COUNT 10
 //#define PALS_COUNT 25
 //#define PALS_COUNT 250
 //#define PALS_COUNT 5000
 //#define PALS_COUNT 10000
 //#define PALS_COUNT 50000
-//#define PALS_COUNT 100000
+#define PALS_COUNT 100000
 //#define PALS_COUNT 1000000
 //#define PALS_COUNT 1500000
 //#define PALS_COUNT 4000000
