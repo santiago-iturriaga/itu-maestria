120cf3075ae95acdcf3f0f6f43bf4401b29b77d0
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-05-07 13:25:00 +0000

    con shifts

--- branches/palsGPU-Lux-Orig/src/main.cu
+++ branches/palsGPU-Lux-Orig/src/main.cu
@@ -2,8 +2,8 @@
 // Name        : palsGPU.cu
 // Author      : Santiago
 // Version     : 1.0
-// Copyright   : 
-// Description : 
+// Copyright   :
+// Description :
 //============================================================================
 
 #include <stdlib.h>
@@ -35,177 +35,177 @@
 
 int main(int argc, char** argv)
 {
-	// =============================================================
-	// Loading input parameters
-	// =============================================================
-	struct params input;
-	if (load_params(argc, argv, &input) == EXIT_FAILURE) {
-		fprintf(stderr, "[ERROR] Ocurrió un error leyendo los parametros de entrada.\n");
-		return EXIT_FAILURE;
-	}
+    // =============================================================
+    // Loading input parameters
+    // =============================================================
+    struct params input;
+    if (load_params(argc, argv, &input) == EXIT_FAILURE) {
+        fprintf(stderr, "[ERROR] Ocurrió un error leyendo los parametros de entrada.\n");
+        return EXIT_FAILURE;
+    }
 
-	// =============================================================
-	// Loading problem instance
-	// =============================================================
-	if (DEBUG) fprintf(stdout, "[DEBUG] Loading problem instance...\n");
-	
-	// Se pide el espacio de memoria para la matriz de ETC.
-	struct matrix *etc_matrix = create_etc_matrix(&input);
+    // =============================================================
+    // Loading problem instance
+    // =============================================================
+    if (DEBUG) fprintf(stdout, "[DEBUG] Loading problem instance...\n");
 
-	// Se carga la matriz de ETC.
-	if (load_instance(&input, etc_matrix) == EXIT_FAILURE) {
-		fprintf(stderr, "[ERROR] Ocurrió un error leyendo el archivo de instancia.\n");
-		return EXIT_FAILURE;
-	}
+    // Se pide el espacio de memoria para la matriz de ETC.
+    struct matrix *etc_matrix = create_etc_matrix(&input);
 
-	//show_etc_matrix(etc_matrix);
-	
-	// =============================================================
-	// Create empty solution
-	// =============================================================
-	if (DEBUG) fprintf(stdout, "[DEBUG] Creating empty solution...\n");
-	struct solution *current_solution = create_empty_solution(etc_matrix);
+    // Se carga la matriz de ETC.
+    if (load_instance(&input, etc_matrix) == EXIT_FAILURE) {
+        fprintf(stderr, "[ERROR] Ocurrió un error leyendo el archivo de instancia.\n");
+        return EXIT_FAILURE;
+    }
 
-	// =============================================================
-	// Solving the problem.
-	// =============================================================
-	if (DEBUG) fprintf(stdout, "[DEBUG] Executing algorithm...\n");
-	
-	// Timming -----------------------------------------------------
-	timespec ts;
-	timming_start(ts);
-	// Timming -----------------------------------------------------
+    //show_etc_matrix(etc_matrix);
 
-	if (input.algorithm == PALS_Serial) {	
-		// =============================================================
-		// Candidate solution
-		// =============================================================
-		if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+    // =============================================================
+    // Create empty solution
+    // =============================================================
+    if (DEBUG) fprintf(stdout, "[DEBUG] Creating empty solution...\n");
+    struct solution *current_solution = create_empty_solution(etc_matrix);
 
-		// Timming -----------------------------------------------------
-		timespec ts_mct;
-		timming_start(ts_mct);
-		// Timming -----------------------------------------------------
+    // =============================================================
+    // Solving the problem.
+    // =============================================================
+    if (DEBUG) fprintf(stdout, "[DEBUG] Executing algorithm...\n");
 
-		compute_mct(etc_matrix, current_solution);
-	
-		// Timming -----------------------------------------------------
-		timming_end(">> MCT Time", ts_mct);
-		// Timming -----------------------------------------------------
-	
-		if (DEBUG) validate_solution(etc_matrix, current_solution);
-		
-		// =============================================================
-		// Serial. Versión de búsqueda completa.
-		// =============================================================
-		pals_serial(input, etc_matrix, current_solution);
-		
-	} else if (input.algorithm == PALS_GPU) {
-		// =============================================================
-		// Candidate solution
-		// =============================================================
-		if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+    // Timming -----------------------------------------------------
+    timespec ts;
+    timming_start(ts);
+    // Timming -----------------------------------------------------
 
-		// Timming -----------------------------------------------------
-		timespec ts_mct;
-		timming_start(ts_mct);
-		// Timming -----------------------------------------------------
+    if (input.algorithm == PALS_Serial) {
+        // =============================================================
+        // Candidate solution
+        // =============================================================
+        if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
 
-		compute_mct(etc_matrix, current_solution);
-	
-		// Timming -----------------------------------------------------
-		timming_end(">> MCT Time", ts_mct);
-		// Timming -----------------------------------------------------
-	
-		if (DEBUG) validate_solution(etc_matrix, current_solution);
-	
-		// =============================================================
-		// CUDA. Versión de búsqueda completa.
-		// =============================================================		
-		
-		gpu_set_device(input.gpu_device);
-		pals_gpu(input, etc_matrix, current_solution);
-		
-	} else if (input.algorithm == PALS_GPU_randTask) {
-		// =============================================================
-		// Candidate solution
-		// =============================================================
-		if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+        // Timming -----------------------------------------------------
+        timespec ts_mct;
+        timming_start(ts_mct);
+        // Timming -----------------------------------------------------
 
-		// Timming -----------------------------------------------------
-		timespec ts_mct;
-		timming_start(ts_mct);
-		// Timming -----------------------------------------------------
+        compute_mct(etc_matrix, current_solution);
 
-		//compute_mct(etc_matrix, current_solution);
-		compute_minmin(etc_matrix, current_solution);
-	
-		// Timming -----------------------------------------------------
-		timming_end(">> MCT Time", ts_mct);
-		// Timming -----------------------------------------------------
-	
-		if (DEBUG) validate_solution(etc_matrix, current_solution);
-	
-		// =============================================================
-		// CUDA. Búsqueda aleatoria por tarea.
-		// =============================================================
-			
-		gpu_set_device(input.gpu_device);
-		pals_gpu_rtask(input, etc_matrix, current_solution);
-			
-	} else if (input.algorithm == PALS_GPU_randMachine) {
-		
-	} else if (input.algorithm == MinMin) {
-		
-		compute_minmin(etc_matrix, current_solution);
-		
-	} else if (input.algorithm == MCT) {
-		
-		compute_mct(etc_matrix, current_solution);
-		
-	} else if (input.algorithm == PALS_GPU_randParallelTask) {
-		// =============================================================
-		// Candidate solution
-		// =============================================================
-		if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+        // Timming -----------------------------------------------------
+        timming_end(">> MCT Time", ts_mct);
+        // Timming -----------------------------------------------------
 
-		// Timming -----------------------------------------------------
-		timespec ts_mct;
-		timming_start(ts_mct);
-		// Timming -----------------------------------------------------
+        if (DEBUG) validate_solution(etc_matrix, current_solution);
 
-		compute_mct(etc_matrix, current_solution);
-	
-		// Timming -----------------------------------------------------
-		timming_end(">> MCT Time", ts_mct);
-		// Timming -----------------------------------------------------
-	
-		if (DEBUG) validate_solution(etc_matrix, current_solution);
-	
-		// =============================================================
-		// CUDA. Búsqueda aleatoria por tarea.
-		// =============================================================
-			
-		gpu_set_device(input.gpu_device);
-		pals_gpu_prtask(input, etc_matrix, current_solution);	
-	}
+        // =============================================================
+        // Serial. Versión de búsqueda completa.
+        // =============================================================
+        pals_serial(input, etc_matrix, current_solution);
 
-	if (OUTPUT_SOLUTION) {
-		//fprintf(stdout, "%d %d\n", etc_matrix->tasks_count, etc_matrix->machines_count);
-		for (int task_id = 0; task_id < etc_matrix->tasks_count; task_id++) {
-			fprintf(stdout, "%d\n", current_solution->task_assignment[task_id]);
-		}
-	}
-	
-	// Timming -----------------------------------------------------
-	timming_end("Elapsed algorithm total time", ts);
-	// Timming -----------------------------------------------------
+    } else if (input.algorithm == PALS_GPU) {
+        // =============================================================
+        // Candidate solution
+        // =============================================================
+        if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
 
-	// =============================================================
-	// Release memory
-	// =============================================================
-	free_etc_matrix(etc_matrix);
-	free_solution(current_solution);
+        // Timming -----------------------------------------------------
+        timespec ts_mct;
+        timming_start(ts_mct);
+        // Timming -----------------------------------------------------
 
-	return EXIT_SUCCESS;
+        compute_mct(etc_matrix, current_solution);
+
+        // Timming -----------------------------------------------------
+        timming_end(">> MCT Time", ts_mct);
+        // Timming -----------------------------------------------------
+
+        if (DEBUG) validate_solution(etc_matrix, current_solution);
+
+        // =============================================================
+        // CUDA. Versión de búsqueda completa.
+        // =============================================================
+
+        gpu_set_device(input.gpu_device);
+        pals_gpu(input, etc_matrix, current_solution);
+
+    } else if (input.algorithm == PALS_GPU_randTask) {
+        // =============================================================
+        // Candidate solution
+        // =============================================================
+        if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+
+        // Timming -----------------------------------------------------
+        timespec ts_mct;
+        timming_start(ts_mct);
+        // Timming -----------------------------------------------------
+
+        compute_mct(etc_matrix, current_solution);
+        //compute_minmin(etc_matrix, current_solution);
+
+        // Timming -----------------------------------------------------
+        timming_end(">> MCT Time", ts_mct);
+        // Timming -----------------------------------------------------
+
+        if (DEBUG) validate_solution(etc_matrix, current_solution);
+
+        // =============================================================
+        // CUDA. Búsqueda aleatoria por tarea.
+        // =============================================================
+
+        gpu_set_device(input.gpu_device);
+        pals_gpu_rtask(input, etc_matrix, current_solution);
+
+    } else if (input.algorithm == PALS_GPU_randMachine) {
+
+    } else if (input.algorithm == MinMin) {
+
+        compute_minmin(etc_matrix, current_solution);
+
+    } else if (input.algorithm == MCT) {
+
+        compute_mct(etc_matrix, current_solution);
+
+    } else if (input.algorithm == PALS_GPU_randParallelTask) {
+        // =============================================================
+        // Candidate solution
+        // =============================================================
+        if (DEBUG) fprintf(stdout, "[DEBUG] Creating initial candiate solution...\n");
+
+        // Timming -----------------------------------------------------
+        timespec ts_mct;
+        timming_start(ts_mct);
+        // Timming -----------------------------------------------------
+
+        compute_mct(etc_matrix, current_solution);
+
+        // Timming -----------------------------------------------------
+        timming_end(">> MCT Time", ts_mct);
+        // Timming -----------------------------------------------------
+
+        if (DEBUG) validate_solution(etc_matrix, current_solution);
+
+        // =============================================================
+        // CUDA. Búsqueda aleatoria por tarea.
+        // =============================================================
+
+        gpu_set_device(input.gpu_device);
+        pals_gpu_prtask(input, etc_matrix, current_solution);
+    }
+
+    if (OUTPUT_SOLUTION) {
+        //fprintf(stdout, "%d %d\n", etc_matrix->tasks_count, etc_matrix->machines_count);
+        for (int task_id = 0; task_id < etc_matrix->tasks_count; task_id++) {
+            fprintf(stdout, "%d\n", current_solution->task_assignment[task_id]);
+        }
+    }
+
+    // Timming -----------------------------------------------------
+    timming_end("Elapsed algorithm total time", ts);
+    // Timming -----------------------------------------------------
+
+    // =============================================================
+    // Release memory
+    // =============================================================
+    free_etc_matrix(etc_matrix);
+    free_solution(current_solution);
+
+    return EXIT_SUCCESS;
 }
