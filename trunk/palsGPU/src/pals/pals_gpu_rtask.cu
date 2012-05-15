2ced9cbec1e8396cc1e7a049f9f4b6577cbddfc3
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-05-15 14:55:48 +0000

    finalllll

--- trunk/palsGPU/src/pals/pals_gpu_rtask.cu
+++ trunk/palsGPU/src/pals/pals_gpu_rtask.cu
@@ -23,10 +23,10 @@
 //#define PALS_RTASK_RANDS                33554432
 
 //#define PALS_GPU_RTASK__BLOCKS          32
-#define PALS_GPU_RTASK__BLOCKS        64
+#define PALS_GPU_RTASK__BLOCKS        128
 //#define PALS_GPU_RTASK__BLOCKS        128
 //#define PALS_GPU_RTASK__LOOPS         1
-#define PALS_GPU_RTASK__LOOPS         8
+#define PALS_GPU_RTASK__LOOPS         1
 //#define PALS_GPU_RTASK__LOOPS         16
 //#define PALS_GPU_RTASK__LOOPS           64
 
