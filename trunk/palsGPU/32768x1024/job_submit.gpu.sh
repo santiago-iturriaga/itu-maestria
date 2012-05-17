e9dd8a1920edaf21d74e13136b964d0917397db7
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-05-17 21:44:25 +0000

    limpieza

--- trunk/palsGPU/32768x1024/job_submit.gpu.sh
+++
@@ -1,1 +0,0 @@
-oarsub -p "host='gaia-61'" -l walltime=20:00:00 -n "pals-gpu" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/run.gpu.sh
--- trunk/palsGPU/32768x1024/job_submit.mct.sh
+++
@@ -1,1 +0,0 @@
-oarsub -p "host='gaia-61'" -l walltime=20:00:00 /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/run.mct.sh
