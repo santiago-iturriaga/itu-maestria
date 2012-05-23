e8605b9219b08c2103d3a9bea50c20d3f474438f
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-05-23 15:33:20 +0000

    fixed... maybe 2

--- trunk/billionGA/src/config.h
+++ trunk/billionGA/src/config.h
@@ -2,13 +2,16 @@
 //#define DEBUG
 //#define TIMMING
 
-#define POPULATION_SIZE     1024
+/*#define POPULATION_SIZE     1024
 #define DELTA               2
 #define MIN_PVALUE          100
-#define MAX_PVALUE          924
+#define MAX_PVALUE          924*/
 
-//#define CPU_LOOP
-#define GPU_LOOP
+#define POPULATION_SIZE     4096
+#define DELTA               1
+#define MIN_PVALUE          0
+#define MAX_PVALUE          4096
+
 
 // The full fitness update model updates the probability vector according
 // to the total sample fitness, but requires an extra barrier to sync the threads.
@@ -17,6 +20,6 @@
 
 #define NUMBER_OF_SAMPLES   2
 
-#define SHOW_UPDATE_EVERY   1000
+#define SHOW_UPDATE_EVERY   100
 
 #define INIT_PROB_VECTOR_VALUE  POPULATION_SIZE >> 1
