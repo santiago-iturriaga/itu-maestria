b3049c8dc5b7f8a8d6a6eaf275a4cc80e757a5ff
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-10-07 19:50:49 +0000

    vuelta a los hilos

--- aedb-mls/test-mls.sh
+++ aedb-mls/test-mls.sh
@@ -1,6 +1,6 @@
 cd /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls
 make clean
-make mls-gdb
+make
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/clusterusers/siturriaga/ns3Files
-~/bin/mpich2-1.4.1p1/bin/mpiexec -np 2 bin/mls 
+~/bin/mpich2-1.4.1p1/bin/mpiexec -np 3 bin/mls 
