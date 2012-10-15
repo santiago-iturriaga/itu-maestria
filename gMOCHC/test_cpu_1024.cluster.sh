3fdbedfbf0900f7d62404e6ae709249e0ed4364c
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-10-15 13:23:38 +0000

    funciona carajo

--- gMOCHC/test_cpu_1024.cluster.sh
+++ gMOCHC/test_cpu_1024.cluster.sh
@@ -10,8 +10,8 @@ SEED=0
 THREADS=8
 TIMEOUT=1
 #ITERATIONS=20
-ITERATIONS=1250
-#ITERATIONS=1000
+#ITERATIONS=1250
+ITERATIONS=2500
 time( bin/gmochc_cpu ~/instancias/1024x32.ME/scenario.0 ~/instancias/1024x32.ME/workload.A.u_c_hihi \
     1024 32 3 ${THREADS} ${SEED} ${TIMEOUT} ${ITERATIONS} 1> sols_1k.txt 2>log_1k.txt)
 #bin/verificador ~/Scheduling/Energy-Makespan/instances.ruso/512x16/scenario.0 ~/Scheduling/Energy-Makespan/instances.ruso/512x16/workload.0 sols.txt 512 16
