399a7ce838566741f53ae23cd2d56b176cf308fc
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-10-23 14:12:12 +0000

    random

--- aedb-mls/run-mls.sh
+++ aedb-mls/run-mls.sh
@@ -1,7 +1,12 @@
+set -x
+
 cd /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls
 make clean
 make
 
+ITERATIONS=10000
+THREADS=12
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/clusterusers/siturriaga/ns3Files
 #time(~/bin/mpich2-1.4.1p1/bin/mpiexec -launcher ssh -launcher-exec oarsh -f $OAR_NODEFILE -np 2 -ppn 1 bin/mls)
-time(~/bin/mpich2-1.4.1p1/bin/mpiexec -launcher ssh -launcher-exec oarsh -f $OAR_NODEFILE -np 9 -ppn 1 bin/mls)
+time(~/bin/mpich2-1.4.1p1/bin/mpiexec -launcher ssh -launcher-exec oarsh -f $OAR_NODEFILE -np 9 -ppn 1 bin/mls $RANDOM $ITERATIONS $THREADS)
