ae88233985f60eb98631f3d061ef6c2da6667dca
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2012-12-11 18:32:11 +0000

    cambios revista

--- trunk/palsGPU/32768x1024/palsGPU.32k.sh
+++ trunk/palsGPU/32768x1024/palsGPU.32k.sh
@@ -9,10 +9,10 @@ mkdir -p ${BASE_PATH}/32768x1024/solutions
 
 ITER=30
 
-TIMEOUT=30
+#TIMEOUT=30
 #TIMEOUT=120
 #TIMEOUT=300
-#TIMEOUT=900
+TIMEOUT=900
 
 TARGET_M_ARRAY=(1996 1979 1980 1982 1971 1973 1991 1991 1994 1997 1975 1974 1978 1988 1972 1979 1991 1986 1975 1991)
 
@@ -24,8 +24,8 @@ for (( p=0; p<ITER; p++ ))
 do
     for (( i=1; i<21; i++ ))
     do
-        TARGET_M=0
-        #TARGET_M=${TARGET_M_ARRAY[i-1]}
+        #TARGET_M=0
+        TARGET_M=${TARGET_M_ARRAY[i-1]}
 
         RAND=$RANDOM
         echo "Random ${RAND}"
@@ -51,12 +51,12 @@ do
         ID_2=3
         THREADS=12
         
-        time (${BASE_PATH}/bin/pals ${INSTANCE}${i}.dat ${DIMENSION} ${ID_1} ${RAND} 0 ${TIMEOUT} ${TARGET_M} ${ID_2} ${THREADS} ${MAX_ITER} \
-            1> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol) \
-            2> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.time
+        #time (${BASE_PATH}/bin/pals ${INSTANCE}${i}.dat ${DIMENSION} ${ID_1} ${RAND} 0 ${TIMEOUT} ${TARGET_M} ${ID_2} ${THREADS} ${MAX_ITER} \
+        #    1> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol) \
+        #    2> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.time
 
-        ${BASE_PATH}/bin/verificador ${INSTANCE}${i}.dat ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol ${DIMENSION} \
-            > ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.makespan
+        #${BASE_PATH}/bin/verificador ${INSTANCE}${i}.dat ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol ${DIMENSION} \
+        #    > ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.makespan
                     
         set +x
     done
--- trunk/palsGPU/32768x1024/palsGPU.job.sh
+++ trunk/palsGPU/32768x1024/palsGPU.job.sh
@@ -1,1 +1,1 @@
-oarsub -p "host='gaia-61'" -l core=12,walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.32k.sh
+oarsub -p "host='gaia-62'" -l core=12,walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.32k.sh
