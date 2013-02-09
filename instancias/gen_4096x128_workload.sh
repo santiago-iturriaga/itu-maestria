e5f8f8bb3d57ebbc2764f969729a67ad87d86186
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-02-09 18:48:12 +0000

    generacion de instancias

---
+++ instancias/gen_4096x128_workload.sh
@@ -0,0 +1,27 @@
+DIMENSIONS="4096 128"
+BASE=$(pwd)
+ME_INSTANCES_PATH="4096x128"
+M_INSTANCES_PATH="4096x128.M"
+SOLUTIONS_DIR="4096x128.ME"
+
+mkdir -p ${SOLUTIONS_DIR}
+
+i=0
+
+cp ${ME_INSTANCES_PATH}/scenario.* ${SOLUTIONS_DIR}
+
+cd ${M_INSTANCES_PATH}
+files=$(ls *)
+cd ${BASE}
+
+for workload_file in ${files}
+do
+    echo "${workload_file} to workload.${i}"
+    
+    cp ${M_INSTANCES_PATH}/${workload_file} ${SOLUTIONS_DIR}/workload.${workload_file}
+    #(python remove_first_line.py ${M_INSTANCES_PATH}/${workload_file}) > ${SOLUTIONS_DIR}/workload.${workload_file}
+    #echo ${EXEC}
+    #${EXEC}
+    
+    i=$((i+1))
+done
