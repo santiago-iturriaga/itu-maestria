faac65ac85a980b0991d15048a0e9aae95f94a92
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-04-20 18:19:46 +0000

    gams 64x4 y 128x8

---
+++ instancias_scheduling/emc/convert_to_gams_128x8.sh
@@ -0,0 +1,1 @@
+python convert_to_gams.py 128 8 128x8/arrival.0 128x8/priorities.0 128x8/workload_high.0 128x8/cores_c8.14 128x8/scenario_c12_high.0 > gams_128x8.txt
---
+++ instancias_scheduling/emc/convert_to_gams_64x4.sh
@@ -0,0 +1,1 @@
+python convert_to_gams.py 64 4 64x4/arrival.0 64x4/priorities.0 64x4/workload_high.0 64x4/cores_c8.11 64x4/scenario_c12_high.2 > gams_64x4.txt
