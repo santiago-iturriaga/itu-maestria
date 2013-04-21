85ee0e3cfb1ec3c2199204998fffa89c9df628ba
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-04-21 21:26:21 +0000

    ...........

---
+++ jmetal-opi/output/verify_16x3.sh
@@ -0,0 +1,2 @@
+python verify_solution.py 16 3 /home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/arrival.0 /home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/priorities.0 /home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/workload_high.0 /home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/cores_c4.19 /home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/scenario_c6_mid.31 VAR
+
