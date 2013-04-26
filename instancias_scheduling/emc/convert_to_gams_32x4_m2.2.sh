39655437ec6fd1dcb3e992e4422d1f44f21283ba
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-04-26 03:58:53 +0000

    Instancias grandes

---
+++ instancias_scheduling/emc/convert_to_gams_32x4_m2.2.sh
@@ -0,0 +1,1 @@
+python convert_to_gams_m2.py 32 4 32x4/arrival.1 32x4/priorities.1 32x4/workload_high.1 32x4/cores_c8.21 32x4/scenario_c10_mid.40 > gams_32x4_m2.2.txt
---
+++ instancias_scheduling/emc/convert_to_gams_32x4_m2.3.sh
@@ -0,0 +1,1 @@
+python convert_to_gams_m2.py 32 4 32x4/arrival.2 32x4/priorities.2 32x4/workload_high.2 32x4/cores_c4.8 32x4/scenario_c4_high.1 > gams_32x4_m2.3.txt
