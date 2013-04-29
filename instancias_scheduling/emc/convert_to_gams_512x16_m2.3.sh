80319e9775919de4839b4a2a3151ada0ce8c66be
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-04-29 04:59:09 +0000

    512x16 gams

---
+++ instancias_scheduling/emc/convert_to_gams_512x16_m2.3.sh
@@ -0,0 +1,1 @@
+python convert_to_gams_m2.py 512 16 512x16/arrival.2 512x16/priorities.2 512x16/workload_high.2 512x16/cores_c8.10 512x16/scenario_c10_mid.4 > gams_512x16_m2.3.txt
---
+++ instancias_scheduling/emc/convert_to_gams_512x16_m2.sh
@@ -0,0 +1,2 @@
+python convert_to_gams_m2.py 512 16 512x16/arrival.0 512x16/priorities.0 512x16/workload_high.0 512x16/cores_c8.1 512x16/scenario_c12_mid.2 > gams_512x16_m2.txt
+
