86a74b9ac8cefe1e93d43448f5c59d088a4197af
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-04-29 04:25:11 +0000

    scripts

--- trunk/gams-opi/verify_16x3_m2.sh
+++ trunk/gams-opi/verify_16x3_m2.sh
@@ -1,8 +1,24 @@
-#INST_PATH=/home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/
-INST_PATH=../emc/16x3/
+INST_PATH=/home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/
+#INST_PATH=../emc/16x3/
 
 echo "WQT--------------"
-python verify_solution.py 16 3 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 ${INST_PATH}cores_c4.19 ${INST_PATH}scenario_c6_mid.31 m2_wqt_assign_16x3.txt m2_wqt_stime_16x3.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 \
+	${INST_PATH}cores_c4.19 ${INST_PATH}scenario_c6_mid.31 m2_wqt_assign_16x3.txt m2_wqt_stime_16x3.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.1 ${INST_PATH}priorities.1 ${INST_PATH}workload_high.1 \
+	${INST_PATH}cores_c4.10 ${INST_PATH}scenario_c6_high.1 m2_wqt_assign_16x3_2.txt m2_wqt_stime_16x3_2.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.2 ${INST_PATH}priorities.2 ${INST_PATH}workload_high.2 \
+	${INST_PATH}cores_c4.16 ${INST_PATH}scenario_c4_high.0 m2_wqt_assign_16x3_3.txt m2_wqt_stime_16x3_3.txt
 
 echo "ENERGY-----------"
-python verify_solution.py 16 3 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 ${INST_PATH}cores_c4.19 ${INST_PATH}scenario_c6_mid.31 m2_nrg_assign_16x3.txt m2_nrg_stime_16x3.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 \
+	${INST_PATH}cores_c4.19 ${INST_PATH}scenario_c6_mid.31 m2_nrg_assign_16x3.txt m2_nrg_stime_16x3.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.1 ${INST_PATH}priorities.1 ${INST_PATH}workload_high.1 \
+	${INST_PATH}cores_c4.10 ${INST_PATH}scenario_c6_high.1 m2_nrg_assign_16x3_2.txt m2_nrg_stime_16x3_2.txt
+
+python verify_solution.py 16 3 ${INST_PATH}arrival.2 ${INST_PATH}priorities.2 ${INST_PATH}workload_high.2 \
+	${INST_PATH}cores_c4.16 ${INST_PATH}scenario_c4_high.0 m2_nrg_assign_16x3_3.txt m2_nrg_stime_16x3_3.txt
--- trunk/gams-opi/verify_8x2_m2.sh
+++ trunk/gams-opi/verify_8x2_m2.sh
@@ -1,4 +1,24 @@
-#INST_PATH=/home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/16x3/
-INST_PATH=../emc/8x2/
+INST_PATH=/home/santiago/google-hosting/itu-maestria/svn/trunk/instancias_scheduling/emc/8x2/
+#INST_PATH=../emc/8x2/
 
-python verify_solution.py 8 2 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 ${INST_PATH}cores_c2.0 ${INST_PATH}scenario_c4_high.1 m2_assignment_8x2.txt m2_starting_time_8x2.txt
+echo "WQT--------------"
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 \
+	${INST_PATH}cores_c2.0 ${INST_PATH}scenario_c4_high.1 m2_wqt_assign_8x2.txt m2_wqt_stime_8x2.txt
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.1 ${INST_PATH}priorities.1 ${INST_PATH}workload_high.1 \
+	${INST_PATH}cores_c4.15 ${INST_PATH}scenario_c6_high.6 m2_wqt_assign_8x2_2.txt m2_wqt_stime_8x2_2.txt
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.2 ${INST_PATH}priorities.2 ${INST_PATH}workload_high.2 \
+	${INST_PATH}cores_c4.21 ${INST_PATH}scenario_c4_high.3 m2_wqt_assign_8x2_3.txt m2_wqt_stime_8x2_3.txt
+
+echo "ENERGY-----------"
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.0 ${INST_PATH}priorities.0 ${INST_PATH}workload_high.0 \
+	${INST_PATH}cores_c2.0 ${INST_PATH}scenario_c4_high.1 m2_nrg_assign_8x2.txt m2_nrg_stime_8x2.txt
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.1 ${INST_PATH}priorities.1 ${INST_PATH}workload_high.1 \
+	${INST_PATH}cores_c4.15 ${INST_PATH}scenario_c6_high.6 m2_nrg_assign_8x2_2.txt m2_nrg_stime_8x2_2.txt
+
+python verify_solution.py 8 2 ${INST_PATH}arrival.2 ${INST_PATH}priorities.2 ${INST_PATH}workload_high.2 \
+	${INST_PATH}cores_c4.21 ${INST_PATH}scenario_c4_high.3 m2_nrg_assign_8x2_3.txt m2_nrg_stime_8x2_3.txt
