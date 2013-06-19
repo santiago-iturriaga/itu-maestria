744418125884aabc2983716a6ce76ed58fdac7f8
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-06-19 00:27:13 +0000

    script update

--- palsGPU/16384x512/palsGPU.16k.sh
+++ palsGPU/16384x512/palsGPU.16k.sh
@@ -1,18 +1,21 @@
-export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusterusers/siturriaga/cuda/lib
+#export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusterusers/siturriaga/cuda/lib
+export LD_LIBRARY_PATH=:/usr/local/cuda/lib64
 
 INSTANCE="/home/clusterusers/siturriaga/instances/Bernabe/palsGPU/etc_c_16384x512_hihi_"
 BASE_PATH="/home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU"
 DIMENSION="16384 512"
 DIMENSION_X="16384x512"
 
+hostname
+
 mkdir -p ${BASE_PATH}/16384x512/solutions
 
 ITER=30
 
-#TIMEOUT=30
+TIMEOUT=30
 #TIMEOUT=120
 #TIMEOUT=300
-TIMEOUT=900
+#TIMEOUT=900
 
 TARGET_M_ARRAY=(1934 1940 1949 1922 1904 1901 1945 1903 1937 1935 1937 1916 1911 1927 1944 1939 1933 1929 1910 1941)
 
@@ -24,13 +27,27 @@ for (( p=0; p<ITER; p++ ))
 do
     for (( i=1; i<21; i++ ))
     do
-        #TARGET_M=0
-        TARGET_M=${TARGET_M_ARRAY[i-1]}
+        TARGET_M=0
+        #TARGET_M=${TARGET_M_ARRAY[i-1]}
         
         RAND=$RANDOM
         echo "Random ${RAND}"
 
         set -x
+
+        echo "=== PALS+gMinMin ==============================================="
+        NAME="pals+gminmin"
+        ID_1=2
+        ID_2=6
+        THREADS=1
+        
+        time (${BASE_PATH}/bin/pals ${INSTANCE}${i}.dat ${DIMENSION} ${ID_1} ${RAND} 0 ${TIMEOUT} ${TARGET_M} ${ID_2} ${THREADS} ${MAX_ITER} \
+            ../mauro-sol/etc_c_${DIMENSION_X}_hihi-${i}.sol \
+            1> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol) \
+            2> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.time
+
+        ${BASE_PATH}/bin/verificador ${INSTANCE}${i}.dat ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol ${DIMENSION} \
+            > ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.makespan
 
         echo "=== PALS+MCT ==============================================="
         NAME="pals+mct"
--- palsGPU/32768x1024/palsGPU.32k.sh
+++ palsGPU/32768x1024/palsGPU.32k.sh
@@ -1,18 +1,21 @@
-export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusterusers/siturriaga/cuda/lib
+#export LD_LIBRARY_PATH=/home/clusterusers/siturriaga/cuda/lib64:/home/clusterusers/siturriaga/cuda/lib
+export LD_LIBRARY_PATH=:/usr/local/cuda/lib64
 
 INSTANCE="/home/clusterusers/siturriaga/instances/Bernabe/palsGPU/etc_c_32768x1024_hihi_"
 BASE_PATH="/home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU"
 DIMENSION="32768 1024"
 DIMENSION_X="32768x1024"
 
+hostname
+
 mkdir -p ${BASE_PATH}/32768x1024/solutions
 
 ITER=30
 
-#TIMEOUT=30
+TIMEOUT=30
 #TIMEOUT=120
 #TIMEOUT=300
-TIMEOUT=900
+#TIMEOUT=900
 
 TARGET_M_ARRAY=(1996 1979 1980 1982 1971 1973 1991 1991 1994 1997 1975 1974 1978 1988 1972 1979 1991 1986 1975 1991)
 
@@ -24,13 +27,27 @@ for (( p=0; p<ITER; p++ ))
 do
     for (( i=1; i<21; i++ ))
     do
-        #TARGET_M=0
-        TARGET_M=${TARGET_M_ARRAY[i-1]}
+        TARGET_M=0
+        #TARGET_M=${TARGET_M_ARRAY[i-1]}
 
         RAND=$RANDOM
         echo "Random ${RAND}"
 
         set -x
+
+        echo "=== PALS+gMinMin ==============================================="
+        NAME="pals+gminmin"
+        ID_1=2
+        ID_2=6
+        THREADS=1
+        
+        time (${BASE_PATH}/bin/pals ${INSTANCE}${i}.dat ${DIMENSION} ${ID_1} ${RAND} 0 ${TIMEOUT} ${TARGET_M} ${ID_2} ${THREADS} ${MAX_ITER} \
+            ../mauro-sol/etc_c_${DIMENSION_X}_hihi-${i}.sol \
+            1> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol) \
+            2> ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.time
+
+        ${BASE_PATH}/bin/verificador ${INSTANCE}${i}.dat ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.sol ${DIMENSION} \
+            > ${BASE_PATH}/${DIMENSION_X}/solutions/${i}.${NAME}.${p}.makespan
 
         echo "=== PALS+MCT ==============================================="
         NAME="pals+mct"
