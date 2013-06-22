331b65f50043a210d004f70532780178869f5d51
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-06-22 15:04:18 +0000

    script update

--- trunk/palsGPU/16384x512/palsGPU.job.sh
+++ trunk/palsGPU/16384x512/palsGPU.job.sh
@@ -1,2 +1,3 @@
-oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-16k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/16384x512/palsGPU.16k.sh
+#oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-16k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/16384x512/palsGPU.16k.sh
+oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-16k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/16384x512/palsGPU.shared.16k.sh
 #oarsub -p "host='gaia-62'" -l core=12,walltime=40:00:00 -n "palsGPU-16k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/16384x512/palsGPU.16k.sh
