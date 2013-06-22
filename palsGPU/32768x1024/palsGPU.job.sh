3fa0ae8082e726bb4b07b11fab6f223074b02c90
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-06-22 15:04:18 +0000

    script update

--- palsGPU/32768x1024/palsGPU.job.sh
+++ palsGPU/32768x1024/palsGPU.job.sh
@@ -1,2 +1,3 @@
-oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.32k.sh
+#oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.32k.sh
+oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.shared.32k.sh
 #oarsub -p "gpu='YES'" -l core=12,walltime=40:00:00 -n "palsGPU-32k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/32768x1024/palsGPU.32k.sh
--- palsGPU/8192x256/palsGPU.job.sh
+++ palsGPU/8192x256/palsGPU.job.sh
@@ -1,1 +1,2 @@
-oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-8k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/8192x256/palsGPU.8k.sh
+#oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-8k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/8192x256/palsGPU.8k.sh
+oarsub -p "gpu='YES'" -l walltime=40:00:00 -n "palsGPU-8k" /home/clusterusers/siturriaga/itu-maestria/trunk/palsGPU/8192x256/palsGPU.shared.8k.sh
