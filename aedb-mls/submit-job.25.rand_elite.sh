2900a8f227b863b2bd6f161504794ea1cbceea36
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-06-29 14:49:45 +0000

    update

--- aedb-mls/submit-job.25.rand_elite.sh
+++ aedb-mls/submit-job.25.rand_elite.sh
@@ -1,1 +1,1 @@
-oarsub -l /nodes=9,walltime=2:00:00 -n "aedb.25.re" /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls/run-mls.25.rand_elite.sh
+oarsub -l /nodes=9,walltime=20:00:00 -n "aedb.25.re" /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls/run-mls.25.rand_elite.sh
