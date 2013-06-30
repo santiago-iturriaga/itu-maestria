aee75d082daaf4f660fc53f190280873ebd750a5
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-06-30 21:44:44 +0000

    test script

--- aedb-mls/test-mls-job.sh
+++ aedb-mls/test-mls-job.sh
@@ -1,1 +1,2 @@
-oarsub -l /nodes=1/core=12,walltime=20:00:00 -n "aedb-mls-test" /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls/test-mls.sh
+#oarsub -l /nodes=1/core=12,walltime=20:00:00 -n "aedb-mls-test" /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls/test-mls.sh
+oarsub -l /nodes=2,walltime=1:00:00 -n "test-mls" /home/clusterusers/siturriaga/itu-maestria/trunk/aedb-mls/test-mls.sh
