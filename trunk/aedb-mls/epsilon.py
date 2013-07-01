24ee1ba9d6802cedf3b416e6f1a1fddfea38a6cd
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-07-01 20:05:39 +0000

    Adding epsilon metric

--- trunk/aedb-mls/epsilon.py
+++ trunk/aedb-mls/epsilon.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  epsilon.py
@@ -23,11 +23,93 @@
 #  
 
 import sys
+import math
+
+def euclidean_distance(point_a, point_b):
+    distance = 0.0
+
+    assert(len(point_a) == len(point_b))
+    dimension = len(point_a)
+
+    for coord in range(dimension):
+        distance = distance + pow(point_a[coord]-point_b[coord],2)
+    
+    return math.sqrt(distance)
+
+def epsilon_metric(ref_pf, comp_pf):
+    assert(len(ref_pf) > 0)
+    assert(len(comp_pf) > 0)
+
+    min_distances = []
+    
+    for comp_sol in comp_pf:
+        min_sol = None
+        min_sol_dist = None
+        
+        for ref_sol in ref_pf:
+            if min_sol is None:
+                min_sol = ref_sol
+                min_sol_dist = euclidean_distance(ref_sol, comp_sol)
+            else:
+                aux_distance = euclidean_distance(ref_sol, comp_sol)
+                
+                if aux_distance < min_sol_dist:
+                    min_sol = ref_sol
+                    min_sol_dist = aux_distance
+
+        min_distances.append(min_sol_dist)
+
+    return max(min_distances)
 
 def main():
-    best_pf=sys.argv[1]
-    comp_pf=sys.argv[2]
-    min_cover=sys.argv[3]
+    if len(sys.argv) != 4:
+        print("[ERROR] Usage: {0} <best PF> <computed PF> <min. coverage>".format(sys.argv[0]))
+        exit(-1)
+    
+    best_pf_file = sys.argv[1]
+    comp_pf_file = sys.argv[2]
+    min_cover = float(sys.argv[3])
+
+    print("Best PF file    : {0}".format(best_pf_file))
+    print("Computed PF file: {0}".format(comp_pf_file))
+    print("Min. coverage   : {0}".format(min_cover))
+    print()
+
+    best_pf = []
+    with open(best_pf_file) as f:
+        for line in f:
+            if len(line.strip()) > 0:
+                data = line.strip().split(" ")
+                assert(len(data)==3)
+
+                if (float(data[1])*-1) >= min_cover:
+                    best_pf.append((float(data[0]),float(data[1])*-1,float(data[2])))
+
+    print("Best PF [count={0}]".format(len(best_pf)))
+    print(best_pf)
+    print()
+
+    comp_pf_final = []
+    with open(comp_pf_file + ".out") as f:
+        for line in f:
+            if len(line.strip()) > 0:
+                data = line.strip().split(",")
+
+                if len(data) == 10:
+                    if (data[0] != "id"):
+                        energy = float(data[-4])
+                        coverage = float(data[-3])
+                        nforwardings = float(data[-2])
+
+                        if coverage > min_cover:
+                            comp_pf_final.append((energy,coverage,nforwardings))
+
+    print("Computed PF [count={0}]".format(len(comp_pf_final)))
+    print(comp_pf_final)
+    print()
+
+    epsilon_value = epsilon_metric(best_pf, comp_pf_final)
+    print("Epsilon = {0:.2f}".format(epsilon_value))
     
     return 0
 
