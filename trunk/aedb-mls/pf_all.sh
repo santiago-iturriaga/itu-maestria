ee949e67e3b18485ab4319dfff216c030df5eda2
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-07-13 15:53:30 +0000

    Adding results d100 and d200

--- trunk/aedb-mls/pf_all.sh
+++ trunk/aedb-mls/pf_all.sh
@@ -1,7 +1,11 @@
-#./fp-eval.py mls-eval-d100-c10/mls-eval-d100-c10 20 10 > pf-d100-c10.log
-#./fp-eval.py mls-eval-d100-c15/mls-eval-d100-c15 20 15 > pf-d100-c15.log
-#./fp-eval.py mls-eval-d100-c20/mls-eval-d100-c20 20 20 > pf-d100-c20.log
+./fp-eval.py mls-eval-d100-c40/mls-eval-d100-c10 20 10 > pf-d100-c40.log
+./fp-eval.py mls-eval-d100-c60/mls-eval-d100-c15 20 15 > pf-d100-c60.log
+./fp-eval.py mls-eval-d100-c80/mls-eval-d100-c20 20 20 > pf-d100-c80.log
 
-./fp-eval.py mls-eval-d200-c10/mls-eval-d200-c10 20 10 > pf-d200-c10.log
-./fp-eval.py mls-eval-d200-c15/mls-eval-d200-c15 20 15 > pf-d200-c15.log
-./fp-eval.py mls-eval-d200-c20/mls-eval-d200-c20 20 20 > pf-d200-c20.log
+./fp-eval.py mls-eval-d200-c40/mls-eval-d200-c20 20 20 > pf-d200-c40.log
+./fp-eval.py mls-eval-d200-c60/mls-eval-d200-c60 5 30 > pf-d200-c60.log
+./fp-eval.py mls-eval-d200-c80/mls-eval-d200-c80 5 40 > pf-d200-c80.log
+
+#./fp-eval.py mls-eval-d300-c40/mls-eval-d200-c40 5 30 > pf-d300-c40.log
+#./fp-eval.py mls-eval-d300-c60/mls-eval-d200-c60 5 45 > pf-d300-c60.log
+#./fp-eval.py mls-eval-d300-c80/mls-eval-d200-c80 5 60 > pf-d300-c80.log
