3dc8b69ed169c2296b3dc78c1bb71571151fbd3e
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-07-15 23:26:47 +0000

    Adding scripts

---
+++ aedb-mls/best_pf/convert_all.sh
@@ -0,0 +1,11 @@
+./convert.py All3Algs100dev.pf 10 > All3Algs100dev-c40.pf 
+./convert.py All3Algs100dev.pf 15 > All3Algs100dev-c60.pf 
+./convert.py All3Algs100dev.pf 20 > All3Algs100dev-c80.pf 
+
+./convert.py All3Algs200dev.pf 20 > All3Algs200dev-c40.pf 
+./convert.py All3Algs200dev.pf 30 > All3Algs200dev-c60.pf 
+./convert.py All3Algs200dev.pf 40 > All3Algs200dev-c80.pf 
+
+./convert.py All3Algs300dev.pf 30 > All3Algs300dev-c40.pf 
+./convert.py All3Algs300dev.pf 45 > All3Algs300dev-c60.pf 
+./convert.py All3Algs300dev.pf 60 > All3Algs300dev-c80.pf
