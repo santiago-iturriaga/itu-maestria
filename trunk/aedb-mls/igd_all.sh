f798ffba3462270c8386c3605bcdb73e2cbec438
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-07-26 15:27:26 +0000

    Scripts

--- trunk/aedb-mls/igd_all.sh
+++ trunk/aedb-mls/igd_all.sh
@@ -47,36 +47,36 @@ echo "igd-d100-c40"
 ./igd-eval-moea.py best-pf-d100-c40.pf ${MOEA}/CellDE/100dev/FUN 30 10 > cellde_igd-d100-c40.log
 ./igd-eval-moea.py best-pf-d100-c40.pf ${MOEA}/NSGAII/100dev/FUN 30 10 > nsgaii_igd-d100-c40.log
 echo "igd-d100-c60"
-./igd-eval-final.py best-pf-d100-c60.pf mls-eval-d100-c60/mls-eval-d100-c15 20 15 > igd-d100-c60.log
+./igd-eval-final.py best-pf-d100-c60.pf mls-eval-d100-c60/mls-eval-d100-c15 20 15 > mls_igd-d100-c60.log
 ./igd-eval-moea.py best-pf-d100-c60.pf ${MOEA}/CellDE/100dev/FUN 30 15 > cellde_igd-d100-c60.log
 ./igd-eval-moea.py best-pf-d100-c60.pf ${MOEA}/NSGAII/100dev/FUN 30 15 > nsgaii_igd-d100-c60.log
 echo "igd-d100-c80"
-./igd-eval-final.py best-pf-d100-c80.pf mls-eval-d100-c80/mls-eval-d100-c20 20 20 > igd-d100-c80.log
+./igd-eval-final.py best-pf-d100-c80.pf mls-eval-d100-c80/mls-eval-d100-c20 20 20 > mls_igd-d100-c80.log
 ./igd-eval-moea.py best-pf-d100-c80.pf ${MOEA}/CellDE/100dev/FUN 30 20 > cellde_igd-d100-c80.log
 ./igd-eval-moea.py best-pf-d100-c80.pf ${MOEA}/NSGAII/100dev/FUN 30 20 > nsgaii_igd-d100-c80.log
 
 echo "igd-d200-c40"
-./igd-eval-final.py best-pf-d200-c40.pf mls-eval-d200-c40/mls-eval-d200-c20 20 20 > igd-d200-c40.log
+./igd-eval-final.py best-pf-d200-c40.pf mls-eval-d200-c40/mls-eval-d200-c20 20 20 > mls_igd-d200-c40.log
 ./igd-eval-moea.py best-pf-d200-c40.pf ${MOEA}/CellDE/200dev/FUN 30 20 > cellde_igd-d200-c40.log
 ./igd-eval-moea.py best-pf-d200-c40.pf ${MOEA}/NSGAII/200dev/FUN 30 20 > nsgaii_igd-d200-c40.log
 echo "igd-d200-c60"
-./igd-eval-final.py best-pf-d200-c60.pf mls-eval-d200-c60/mls-eval-d200-c60 20 30 > igd-d200-c60.log
+./igd-eval-final.py best-pf-d200-c60.pf mls-eval-d200-c60/mls-eval-d200-c60 20 30 > mls_igd-d200-c60.log
 ./igd-eval-moea.py best-pf-d200-c60.pf ${MOEA}/CellDE/200dev/FUN 30 30 > cellde_igd-d200-c60.log
 ./igd-eval-moea.py best-pf-d200-c60.pf ${MOEA}/NSGAII/200dev/FUN 30 10 > nsgaii_igd-d200-c60.log
 echo "igd-d200-c80"
-./igd-eval-final.py best-pf-d200-c80.pf mls-eval-d200-c80/mls-eval-d200-c80 20 40 > igd-d200-c80.log
+./igd-eval-final.py best-pf-d200-c80.pf mls-eval-d200-c80/mls-eval-d200-c80 20 40 > mls_igd-d200-c80.log
 ./igd-eval-moea.py best-pf-d200-c80.pf ${MOEA}/CellDE/200dev/FUN 30 40 > cellde_igd-d200-c80.log
 ./igd-eval-moea.py best-pf-d200-c80.pf ${MOEA}/NSGAII/200dev/FUN 30 40 > nsgaii_igd-d200-c80.log
 
 echo "igd-d300-c40"
-./igd-eval-final.py best-pf-d300-c40.pf mls-eval-d300-c40/mls-eval-d300-c40-1 10 30 > igd-d300-c40.log
+./igd-eval-final.py best-pf-d300-c40.pf mls-eval-d300-c40/mls-eval-d300-c40-1 10 30 > mls_igd-d300-c40.log
 ./igd-eval-moea.py best-pf-d200-c40.pf ${MOEA}/CellDE/300dev/FUN 30 30 > cellde_igd-d200-c40.log
 ./igd-eval-moea.py best-pf-d200-c40.pf ${MOEA}/NSGAII/300dev/FUN 30 30 > nsgaii_igd-d200-c40.log
 echo "igd-d300-c60"
-./igd-eval-final.py best-pf-d300-c60.pf mls-eval-d300-c60/mls-eval-d300-c60-1 8 45 > igd-d300-c60.log
+./igd-eval-final.py best-pf-d300-c60.pf mls-eval-d300-c60/mls-eval-d300-c60-1 8 45 > mls_igd-d300-c60.log
 ./igd-eval-moea.py best-pf-d200-c60.pf ${MOEA}/CellDE/300dev/FUN 30 45 > cellde_igd-d200-c60.log
 ./igd-eval-moea.py best-pf-d200-c60.pf ${MOEA}/NSGAII/300dev/FUN 30 45 > nsgaii_igd-d200-c60.log
 echo "igd-d300-c80"
-./igd-eval-final.py best-pf-d300-c80.pf mls-eval-d300-c80/mls-eval-d300-c80-1 5 60 > igd-d300-c80.log
+./igd-eval-final.py best-pf-d300-c80.pf mls-eval-d300-c80/mls-eval-d300-c80-1 5 60 > mls_igd-d300-c80.log
 ./igd-eval-moea.py best-pf-d100-c80.pf ${MOEA}/CellDE/300dev/FUN 30 60 > cellde_igd-d100-c80.log
 ./igd-eval-moea.py best-pf-d100-c80.pf ${MOEA}/NSGAII/300dev/FUN 30 60 > nsgaii_igd-d100-c80.log
