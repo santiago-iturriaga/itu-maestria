971b1f1b8b5861bf2e264ef22461bbf46bc28932
santiago.iturriaga <santiago.iturriaga@04019538-fc05-46f8-052f-b547bbfd49f0>
2013-09-27 01:38:04 +0000

    new exec

--- trunk/aedb-mls/pf_all.sh
+++ trunk/aedb-mls/pf_all.sh
@@ -1,14 +1,14 @@
-./fp-eval.py mls-eval-d100-c40/mls-eval-d100-c10 20 10 > pf-d100-c40.log
-./fp-eval.py mls-eval-d100-c60/mls-eval-d100-c15 20 15 > pf-d100-c60.log
-./fp-eval.py mls-eval-d100-c80/mls-eval-d100-c20 20 20 > pf-d100-c80.log
+./fp-eval.py mls-eval-d100-c40/mls-eval-d100-c40 20 10 > pf-d100-c40.log
+./fp-eval.py mls-eval-d100-c60/mls-eval-d100-c60 20 15 > pf-d100-c60.log
+./fp-eval.py mls-eval-d100-c80/mls-eval-d100-c80 20 20 > pf-d100-c80.log
 
-./fp-eval.py mls-eval-d200-c40/mls-eval-d200-c20 20 20 > pf-d200-c40.log
-./fp-eval.py mls-eval-d200-c60/mls-eval-d200-c60 14 30 > pf-d200-c60.log
-./fp-eval.py mls-eval-d200-c80/mls-eval-d200-c80 10 40 > pf-d200-c80.log
+./fp-eval.py mls-eval-d200-c40/mls-eval-d200-c40 20 20 > pf-d200-c40.log
+./fp-eval.py mls-eval-d200-c60/mls-eval-d200-c60 20 30 > pf-d200-c60.log
+./fp-eval.py mls-eval-d200-c80/mls-eval-d200-c80 20 40 > pf-d200-c80.log
 
-./fp-eval.py mls-eval-d300-c40/mls-eval-d300-c40-1 5 30 > pf-d300-c40.log
-./fp-eval.py mls-eval-d300-c60/mls-eval-d300-c60-1 5 45 > pf-d300-c60.log
-./fp-eval.py mls-eval-d300-c80/mls-eval-d300-c80-1 5 60 > pf-d300-c80.log
+./fp-eval.py mls-eval-d300-c40/mls-eval-d300-c40 10 30 > pf-d300-c40.log
+./fp-eval.py mls-eval-d300-c60/mls-eval-d300-c60 10 45 > pf-d300-c60.log
+./fp-eval.py mls-eval-d300-c80/mls-eval-d300-c80 10 60 > pf-d300-c80.log
 
 ./fp-best.py best_pf/All3Algs100dev-c40.pf pf-d100-c40.log > best-pf-d100-c40.pf
 ./fp-best.py best_pf/All3Algs100dev-c60.pf pf-d100-c60.log > best-pf-d100-c60.pf
